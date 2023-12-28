7d83bdad504516e01d7ae84dcc8151643cb58bbd
tatyu <tatyu@t.local>
2023-12-28 11:09:11 +0900

    :construction: Work in progress.

--- include/config_parser/conf.hpp
+++ include/config_parser/conf.hpp
@@ -3,117 +3,87 @@
 
 #include <iostream>
 #include <vector>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-/*
- * context level:
- * main -> http -> server -> location
- * 
-**/
-
-struct CErrorLog
+class listening
 {
-	enum errorlog_level
-	{
-		DEBUG,
-		INFO,
-		NOTICE,
-		WARN,
-		ERROR,
-		CRIT,
-		ALERT,
-		EMERG
-	};
-	errorlog_level	level_;
-};
-
-// level: http, server, location
-struct CErrorPage
-{
-	std::vector<unsigned int>	statuses_;
-	std::string	uri_;
-};
-
-
-// level: server
-struct CServerListen
-{
-	std::string	address_; // default:0.0.0.0
-	unsigned int	port_; // default:80
-};
-
-// serverの要素
-struct CServerLocation
-{
-	std::string	root_;
-	std::string	uri_;
-	
-};
-
-// level: http, server, location
-struct CErrorPage
-{
-	std::vector<unsigned int>	statuses_;
-	int	response_; //optional
-	std::string uri_;
-};
-
-// level: http, server, location
-struct CPermission
-{
-	std::vector<std::string> allow_;
-	std::vector<std::string> deny_;
-};
-
-// level: location
-struct CLocationLimitExpect
-{
-	std::string	method_;
-	CPermission	permission_;
-};
-
-// level: server
-struct CServerLocation
-{
-	std::string	root_;
-	std::string	uri_;
-	unsigned int	send_timeout_; // default:60s
-	unsigned int	keepalive_timeout_; // default:75s
-	unsigned int	client_max_body_size_; // Default:1m, sizeを0にすると制限なし 
-	bool	autoindex_;
-	std::string	index_;
-	CErrorPage	error_page_;
-};
-
-// serverの設定
-struct CServer
-{
-	std::string	root_;
-	std::vector<std::string>	server_names_;
-	CServerListen	listen_;
-	std::vector<CServerLocation>	locations_;
-	bool	autoindex_;
-	CErrorPage	error_page_;
-	std::string	index_;
-};
-
-// httpの設定
-struct CHTTP
-{
-	std::string	root_;
-	std::vector<CServer>	servers_;
-	CErrorPage	error_page_;
-	bool	autoindex_;
-	std::string	index_;
-};
-
-class Conf
-{
-	private: // level: main
-		CErrorLog	errorlog_;
-		CHTTP	http_;
+	private:
+		int	socketfd_;
+		struct sockaddr	*sockaddr_;
+		socklen_t	socklen_; /* size of sockaddr */
+		size_t	addr_text_max_len_;
+		std::string	addr_;
 	public:
-		Conf();
-		~Conf();
+		listening(/* args */);
+		~listening();
+};
+
+class Connection
+{
+	private:
+		long	timeout;
+		event	read;
+		event	write;
+		log	error_log;
+		listening	listening;
+	public:
+	Connection();
+	~Connection();
+};
+
+class LocationContext
+{
+private:
+	/* data */
+public:
+	LocationContext();
+	~LocationContext();
+};
+
+
+class ServerContext
+{
+private:
+	/* data */
+public:
+	ServerContext();
+	~ServerContext();
+};
+
+
+class MainContext
+{
+private:
+	/* data */
+public:
+	MainContext();
+	~MainContext();
+};
+
+
+class directive
+{
+private:
+	enum context
+	{
+		MAIN,
+		CONNECTION,
+		HTTP,
+		SERVER,
+		LOCATION,
+	};
+	std::string name;
+	std::vector<std::string> args;
+	context	context;
+};
+
+class context
+{
+private:
+	std::string name;
+	std::vector<class directive> directives;
+	std::vector<class context> children;
 };
 
 #endif
