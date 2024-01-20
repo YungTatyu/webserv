9877bc3c0642c98734a245db29d5721e03d0001d
tatyu <tatyu@t.local>
2024-01-20 12:14:09 +0900

    context class to struct

--- src/config/LimitExcept.hpp
+++ src/config/LimitExcept.hpp
@@ -11,15 +11,14 @@
 
 namespace config
 {
-class LimitExcept
+struct LimitExcept
 {
-	public:
-		LimitExcept(const config::CONNECTION_METHOD method) : excepted_method_(method) {}
-		~LimitExcept() {}
-		std::set<std::string>	set_directives;
-		const config::CONNECTION_METHOD	excepted_method_; // limit_exceptの制限から除外されるmethod
-		std::vector<Allow>	allow_list;
-		std::vector<Deny>	deny_list;
+	LimitExcept(const config::CONNECTION_METHOD method) : excepted_method_(method) {}
+	~LimitExcept() {}
+	std::set<std::string>	set_directives;
+	const config::CONNECTION_METHOD	excepted_method_; // limit_exceptの制限から除外されるmethod
+	std::vector<Allow>	allow_list;
+	std::vector<Deny>	deny_list;
 };
 } // namespace config
 
