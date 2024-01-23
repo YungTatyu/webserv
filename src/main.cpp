fc07beda5eb40084b456fd03b072528b64b82e97
tatyu <tatyu@t.local>
2024-01-23 11:57:03 +0900

    config_parser test雛形作成

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <cstdlib>
-#include "Parser.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	std::cout << "Hello webserv" << std::endl;
+	(void) argc;
+	std::cerr << "Hello webserv " << argv[1] << std::endl;
 
-	std::vector<config::Token>	tokens_;
-	config::Parser	parser(tokens_, "filepath");
+	// std::vector<config::Token>	tokens_;
+	// config::Parser	parser(tokens_, "filepath");
 
-	char **arg;
+	// char **arg;
 
-	arg = (char **)malloc(sizeof(char *) * 2);
-	arg[0] = strdup("cgi_exe");
-	arg[1] = NULL;
-	execve("cgi/cgi_exe", arg, NULL);
+	// arg = (char **)malloc(sizeof(char *) * 2);
+	// arg[0] = strdup("cgi_exe");
+	// arg[1] = NULL;
+	// execve("cgi/cgi_exe", arg, NULL);
 	return (0);
 }
