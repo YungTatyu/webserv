94cdb18d7529ba038bfeacc7a4fce018fe2714d6
tatyu <tatyu@t.local>
2024-01-25 15:22:20 +0900

    conf_parser test init

--- test/conf/conf_parser_test.sh
+++ test/conf/conf_parser_test.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# init
+readonly script_dir=$(dirname "$0")
+readonly webserb_path="${script_dir}/../../webserv"
+if [ -e $webserb_path ]
+then
+	printf "|------------------ webserv test start ------------------|\n\n"
+else
+	echo "${webserb_path}: command not found"
+	echo "run make first to test"
+	exit 1
+fi
+
 g_test_index=0
 
 function	assert {
 	g_test_index=$(bc <<< "$g_test_index + 1")
 	local	conf_path=$1
 	printf "test$g_test_index $conf_path: "
-	local	actual=$(../../webserv $conf_path 2>&1)
+	local	actual=$(${webserb_path} $conf_path 2>&1)
 	local	expect=$2
 
 	if [ "$actual" = "$expect" ]
@@ -21,10 +33,10 @@ function	assert {
 
 readonly err_start_with="webserv: [emerg]"
 
-conf_path="./conf_files/error/double_quote_not_closed.conf"
+conf_path="test/conf/conf_files/error/double_quote_not_closed.conf"
 err_path="in $(readlink -f $conf_path)"
 assert $conf_path "${err_start_with} unexpected end of file, expecting \";\" or \"}\" ${err_path}:2\n"
 
-conf_path="./conf_files/error/double_quote_not_closed.conf"
+conf_path="test/conf/conf_files/error/double_quote_not_closed.conf"
 err_path="in $(readlink -f $conf_path)"
 assert $conf_path "${err_start_with} unexpected end of file, expecting \";\" or \"}\" ${err_path}:2\n"
