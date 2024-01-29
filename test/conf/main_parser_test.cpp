09f6f7232786d356fa0cb8eaf29289133c43e379
tatyu <tatyu@t.local>
2024-01-30 00:32:32 +0900

    main parser test added

---
+++ test/conf/main_parser_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "conf.hpp"
+
+namespace test
+{
+void	init_config(const std::string &path)
+{
+	config::init_config(path);
+}
+} // namespace test
+
+
+TEST(mainParserTest, )
+{
+	const char	*filepath = "";
+}
