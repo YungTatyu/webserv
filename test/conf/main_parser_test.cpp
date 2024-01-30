74ac9ed5d789dd3af822e79db3729291caebaa0b
tatyu <tatyu@t.local>
2024-01-30 16:02:19 +0900

    main parser test added

--- test/conf/main_parser_test.cpp
+++ test/conf/main_parser_test.cpp
@@ -1,16 +1,22 @@
 #include <gtest/gtest.h>
 #include "conf.hpp"
+#include "Main.hpp"
 
 namespace test
 {
-void	init_config(const std::string &path)
-{
-	config::init_config(path);
-}
 } // namespace test
 
 
-TEST(mainParserTest, )
+const static std::string	HTML = "html";
+const static std::string	EVENTS = "events";
+const static std::string	SERVER = "server";
+const static std::string	LOCATION = "location";
+const static std::string	LIMIT_EXCEPT = "limit_except";
+
+TEST(mainParserTest, allContexts)
 {
-	const char	*filepath = "";
+	const Main	*config = config::init_config("conf_files/valid/12.conf");
+
+	// http
+	EXPECT_NOT_EQ(config->directives_set.find(HTML), config->directives_set.end());
 }
