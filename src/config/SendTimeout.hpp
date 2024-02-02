c5ce994b289433c8aba05a2ded912f4252d06fa4
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-03 03:12:57 +0900

    :bug: send_timeout is allowed in http context

--- src/config/SendTimeout.hpp
+++ src/config/SendTimeout.hpp
@@ -16,7 +16,7 @@ class SendTimeout
 		const static unsigned long	kDefaultTime_;
 		const Time	&getTime() const { return this->time_; }
 		void	setTime(const unsigned long &time_in_ms) { this->time_.time_in_ms_ = time_in_ms;}
-		const static unsigned int	kType_ = CONF_HTTP_LOCATION|CONF_TAKE1|CONF_UNIQUE;
+		const static unsigned int	kType_ = CONF_HTTP|CONF_TAKE1|CONF_UNIQUE;
 };
 } // namespace config
 
