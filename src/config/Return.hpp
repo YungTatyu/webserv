c1816a1c010dde865f56cf136107c0fa7804881e
tatyu <tatyu@t.local>
2024-02-03 22:09:56 +0900

    :seedling:return test added

--- src/config/Return.hpp
+++ src/config/Return.hpp
@@ -15,9 +15,9 @@ class Return
 	public:
 		Return() : code_(this->kCodeUnset) {}
 		~Return() {}
-		const unsigned int	&getCode() const { return this->code_; }
+		const int	&getCode() const { return this->code_; }
 		const std::string	&getUrl() const { return this->url_; }
-		void	setCode(const unsigned int &code) { this->code_ = code; }
+		void	setCode(const int &code) { this->code_ = code; }
 		void	setUrl(const std::string &url) { this->url_ = url; }
 		const static unsigned int	kType_ = CONF_HTTP_LOCATION|CONF_TAKE12|CONF_NOT_UNIQUE;
 		const static int kCodeUnset = -1;
--- test/conf/directives_parser/return/return_test.cpp
+++ test/conf/directives_parser/return/return_test.cpp
@@ -13,24 +13,21 @@
 namespace test
 {
 void	test_value(
-	const std::vector<config::Return> &Return_list,
-	const std::vector<std::string> &address_list,
-	const std::vector<unsigned int> &port_list,
-	const std::vector<bool> &default_server_list
+	const std::vector<config::Return> &return_list,
+	const std::vector<int> &code_list,
+	const std::vector<std::string> &url_list
 )
 {
-	int	li = 0; // Return index
-	std::for_each(Return_list.begin(), Return_list.end(),
-		[&li, &address_list, &port_list, &default_server_list](config::Return Return){
-		// test address
-		EXPECT_EQ(Return.getAddress(), address_list[li]);
+	int	i = 0; // Return index
+	std::for_each(return_list.begin(), return_list.end(),
+		[&i, &code_list, &url_list](config::Return return_){
+		// test code
+		EXPECT_EQ(return_.getCode(), code_list[i]);
 
-		// test port
-		EXPECT_EQ(Return.getport(), port_list[li]);
+		// test url
+		EXPECT_EQ(return_.getUrl(), url_list[i]);
 
-		// test uri
-		EXPECT_EQ(Return.getIsDefaultServer(), default_server_list[li]);
-		++li;
+		++i;
 	});
 }
 } // namespace test
@@ -47,12 +44,11 @@ TEST(ReturnTest, allContext)
 	const std::vector<config::Server>	&server_list = http.server_list;
 
 	// location
-	test::test_value(http.server_list[0].location_list[0].Return_list,
-		{"1.1.1.1", "127.1.1.1", "2.2.2.2", "3.3.3.3", "127.1.1.1", "127.1.1.1", "127.1.1.1", "127.1.1.1"},
-		{80, 1, 2000, 80, 2400, 80, 1000, 65535},
-		{false, false, false, true, true, false, false, true}
+	test::test_value(http.server_list[0].location_list[0].return_list,
+		{301, -1, 999, 998, 0, 0},
+		{"url", "https://www.tachu.com", "url999", "1000", "", ""}
 	);
-	test::test_directives_set(http.server_list[0].directives_set, kReturn, true);
+	test::test_directives_set(http.server_list[0].location_list[0].directives_set, kReturn, true);
 }
 
 TEST(ReturnTest, notFound) {
@@ -64,5 +60,5 @@ TEST(ReturnTest, notFound) {
 	const config::Events	&events = config->events;
 	const std::vector<config::Server>	&server_list = http.server_list;
 
-	test::test_directives_set(http.server_list[0].directives_set, kReturn, false);
+	test::test_directives_set(http.server_list[0].location_list[0].directives_set, kReturn, false);
 }
