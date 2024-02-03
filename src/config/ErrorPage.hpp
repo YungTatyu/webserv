828358ccbd7853d795c82c15caebd3cd854fbe03
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-03 18:15:53 +0900

    :zap: initialize member valiables

--- src/config/ErrorPage.hpp
+++ src/config/ErrorPage.hpp
@@ -15,7 +15,7 @@ class ErrorPage
 		long					response_;
 		std::string				uri_;
 	public:
-		ErrorPage() : response_(-1) {}
+		ErrorPage() : response_(this->kResponseUnset) {}
 		~ErrorPage() {}
 		const std::set<unsigned int>	&getCodeList() const { return this->code_set_; }
 		const long	&getResponse() const { return this->response_; }
@@ -24,6 +24,7 @@ class ErrorPage
 		void	setResponse(const long &response) { this->response_ = response; }
 		void	setUri(const std::string &uri) { this->uri_ = uri; }
 		const static unsigned int	kType_ = CONF_HTTP|CONF_HTTP_SERVER|CONF_HTTP_LOCATION|CONF_1MORE|CONF_NOT_UNIQUE;
+		const static long			kResponseUnset = -1;
 };
 } // namespace config
 
