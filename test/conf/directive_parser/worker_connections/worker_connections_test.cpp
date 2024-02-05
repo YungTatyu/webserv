a2786b75e516c41a72f6adaf3a281858975b9125
tatyu <tatyu@t.local>
2024-02-05 15:21:26 +0900

    :seedling:worker_connections test added

---
+++ test/conf/directive_parser/worker_connections/worker_connections_test.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <string>
+
+#include "conf.hpp"
+#include "Main.hpp"
+#include "directives_test.hpp"
+
+const std::string	kWorkerConnections = "worker_connections";
+
+TEST(WorkerConnectionsTest, max)
+{
+	const config::Main	*config = config::init_config("test/conf/directive_parser/worker_connections/1.conf");
+	ASSERT_NE(config, nullptr);
+
+	const config::Http	&http = config->http;
+	const config::Events	&events = config->events;
+	const std::vector<config::Server>	&server_list = http.server_list;
+
+	EXPECT_EQ(events.worker_connections.getWorkerConnections(), 9223372036854775807);
+	test::test_directives_set(events.directives_set, kWorkerConnections, true);
+}
+
+TEST(WorkerConnectionsTest, random)
+{
+	const config::Main	*config = config::init_config("test/conf/directive_parser/worker_connections/2.conf");
+	ASSERT_NE(config, nullptr);
+
+	const config::Http	&http = config->http;
+	const config::Events	&events = config->events;
+	const std::vector<config::Server>	&server_list = http.server_list;
+
+	EXPECT_EQ(events.worker_connections.getWorkerConnections(), 9223372036854775806);
+	test::test_directives_set(events.directives_set, kWorkerConnections, true);
+}
+
+TEST(WorkerConnectionsTest, min)
+{
+	const config::Main	*config = config::init_config("test/conf/directive_parser/worker_connections/3.conf");
+	ASSERT_NE(config, nullptr);
+
+	const config::Http	&http = config->http;
+	const config::Events	&events = config->events;
+	const std::vector<config::Server>	&server_list = http.server_list;
+
+	EXPECT_EQ(events.worker_connections.getWorkerConnections(), 2);
+	test::test_directives_set(events.directives_set, kWorkerConnections, true);
+}
+
+
+
+TEST(WorkerConnectionsTest, notFound) {
+		
+	const config::Main	*config = config::init_config("test/conf/directive_parser/only_context.conf");
+	ASSERT_NE(config, nullptr);
+
+	const config::Http	&http = config->http;
+	const config::Events	&events = config->events;
+	const std::vector<config::Server>	&server_list = http.server_list;
+
+	test::test_directives_set(events.directives_set, kWorkerConnections, false);
+}
