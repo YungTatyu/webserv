a5b185f18405ce24a561efb22987e005d6ab28c8
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-09 17:16:00 +0900

    :bug: modify test class

--- test/server/server_config_test.cpp
+++ test/server/server_config_test.cpp
@@ -2,72 +2,73 @@
 #include <gtest/gtest-param-test.h>
 #include "Lexer.hpp"
 #include "Parser.hpp"
-#include "Webserver.hpp"
+#include "ServerConfig.hpp"
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
 
-class TestConfig : public ::testing::Test {
+class ServerConfigTest : public ::testing::Test {
 protected:
     // 各TESTの前に呼び出されるセットアップメソッド
 	void SetUp() override {
 		std::string		file_path = "test/server/conf_files/server_config_test.conf";
-		config::Main	*config = new Main();
-		config::Lexer	lexern(file_path);
+		config::Main	*config = new config::Main();
+		config::Lexer	lexer(file_path);
 		lexer.tokenize();
 
-		confif::Parser	parser(*config, lexer.getTokens(), file_path);
+		config::Parser	parser(*config, lexer.getTokens(), file_path);
+		parser.parse();
 
-		server_config_.loadConfig(config);
+		server_config_.loadConfiguration(config);
 	}
 
 	// テストに使う共有のオブジェクト
 	ServerConfig	server_config_;
 };
 
-TEST(ServerConfigTest, getServer)
+TEST_F(ServerConfigTest, getServer1)
 {
 	config::Server&	server;
 
 	server = server_config_.getServer("first_server", "127.0.0.1", 8001);
 
-	EXPECT_true(server_config_.config_.http.server[0], server);
+	EXPECT_EQ(server_config_.config_->http.server_list[0], server);
 }
 
-TEST(ServerConfigTest, getServer)
+TEST_F(ServerConfigTest, getServer2)
 {
 	config::Server&	server;
 
 	server = server_config_.getServer("second_server", "127.0.0.2", 8002);
 
-	EXPECT_true(server_config_.config_.http.server[1], server);
+	EXPECT_EQ(server_config_.config_->http.server_list[1], server);
 }
 
-TEST(ServerConfigTest, getServer)
+TEST_F(ServerConfigTest, getServer3)
 {
 	config::Server&	server;
 
 	server = server_config_.getServer("third_server", "127.0.0.3", 8003);
 
-	EXPECT_true(server_config_.config_.http.server[2], server);
+	EXPECT_EQ(server_config_.config_->http.server_list[2], server);
 }
 
-TEST(ServerConfigTest, getServer)
+TEST_F(ServerConfigTest, getServer4)
 {
 	config::Server&	server;
 
 	server = server_config_.getServer("default_server", "127.0.0.10", 8080);
 
 	// server[1]がデフォルトサーバー
-	EXPECT_true(server_config_.config_.http.server[1], server);
+	EXPECT_EQ(server_config_.config_->http.server_list[1], server);
 }
 
-TEST(ServerConfigTest, getLocation)
+TEST_F(ServerConfigTest, getLocatio1)
 {
 	config::Location&	location;
 
 	location = server_config_.getLocation(server_config_.http.server[0], "hello");
 
-	EXPECT_true(server_config_.config_.http.server[0].location[1], location);
+	EXPECT_EQ(server_config_.config_->http.server_list[0].location_list[1], location);
 }
 
