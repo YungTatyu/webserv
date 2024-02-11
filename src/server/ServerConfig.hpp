27713ba2bf3a6601758094913ffd56c46136284f
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-11 13:48:22 +0900

    :zap: add allowRequest test

--- src/server/ServerConfig.hpp
+++ src/server/ServerConfig.hpp
@@ -10,6 +10,45 @@
  * この場合、すべてのクラスがこのクラスのオブジェクトを持たなければならない
 */ 
 
+/**
+ *IPv4
+ *
+  typedef __uint32_t      in_addr_t;      // base type for internet address
+  typedef __uint16_t      in_port_t;
+
+  struct in_addr {
+        in_addr_t s_addr;
+  };
+
+  struct sockaddr_in {
+        __uint8_t       sin_len;
+        sa_family_t     sin_family;
+        in_port_t       sin_port;
+        struct  in_addr sin_addr;
+        char            sin_zero[8];
+  };
+  *
+  *
+  *IPv6
+  *
+  struct in6_addr {
+        union {
+                __uint8_t   __u6_addr8[16];
+                __uint16_t  __u6_addr16[8];
+                __uint32_t  __u6_addr32[4];
+        } __u6_addr;                    // 128-bit IP6 address
+  };
+
+  struct sockaddr_in6 {
+        __uint8_t       sin6_len;       // length of this struct(sa_family_t)
+        sa_family_t     sin6_family;    // AF_INET6 (sa_family_t)
+        in_port_t       sin6_port;      // Transport layer port # (in_port_t)
+        __uint32_t      sin6_flowinfo;  // IP6 flow information
+        struct in6_addr sin6_addr;      // IP6 address 
+        __uint32_t      sin6_scope_id;  // scope zone index
+  };
+ */
+
 /* Confファイルの設定を管理する */
 class ServerConfig
 {
@@ -22,20 +61,36 @@ class ServerConfig
 		void	loadConfiguration( const config::Main* config );
 
 		// method
-		bool	allowRequest( const std::string& server_name, const std::string& address, const unsigned int port ) const;
-		const std::string&	getFile( const std::string& server_name, const std::string& address, const unsigned int port ) const;
-		void	writeAcsLog( const std::string& server_name, const std::string& address, const unsigned int port );
-		void	writeErrLog( const std::string& server_name, const std::string& address, const unsigned int port );
-		const config::Time&	getKeepaliveTimeout( const std::string& server_name, const std::string& address, const unsigned int port );
-		const config::Time&	getSendTimeout( const std::string& server_name, const std::string& address, const unsigned int port );
-		const config::Time&	getUseridExpires( const std::string& server_name, const std::string& address, const unsigned int port );
+		// cli_addrのデータ型どうするか？これだとipv4しか対応できない。
+		bool	allowRequest( const std::string& server_name,
+							const std::string& serv_addr,
+							const unsigned int 
+							const std::string& uri,
+							const in_addr_t cli_addr ) const;
+		const std::string&	getFile( const std::string& server_name,
+									const std::string& address,
+									const unsigned int port ) const;
+		void	writeAcsLog( const std::string& server_name,
+							const std::string& address,
+							const unsigned int port );
+		void	writeErrLog( const std::string& server_name,
+							const std::string& address,
+							const unsigned int port );
+		const config::Time&	getKeepaliveTimeout( const std::string& server_name,
+												const std::string& address,
+												const unsigned int port );
+		const config::Time&	getSendTimeout( const std::string& server_name,
+											const std::string& address,
+											const unsigned int port );
+		const config::Time&	getUseridExpires( const std::string& server_name,
+											const std::string& address,
+											const unsigned int port );
 		const config::Size&	getClientMaxBodySize();
 
 	private:
 		//utils
 		config::Server&	getServer( const std::string& server_name, const std::string& address, const unsigned int port );
 		config::Location&	getLocation( const config::Server&server, const std::string& path );
-		// const std::string&	getFullPath( const config::Server& server, const config::Location& location );
 
 	public:
 		int		getServPort();
--- test/server/server_config_test.cpp
+++ test/server/server_config_test.cpp
@@ -32,14 +32,42 @@ protected:
 	ServerConfig	server_config_;
 };
 
+// allowRequestの引数プラスでclient自身のアドレス必要かも
 TEST_F(ServerConfigTest, allowRequest)
 {
-	EXPECT_FALSE(server_config_.allowRequest());
-	EXPECT_FALSE(server_config_.allowRequest());
-	EXPECT_FALSE(server_config_.allowRequest());
-	EXPECT_FALSE(server_config_.allowRequest());
-	EXPECT_FALSE(server_config_.allowRequest());
-	EXPECT_FALSE(server_config_.allowRequest());
+	in_addr_t	cli_addr1 = 3232235521; // 192.168.0.1の３２bit整数表現
+	in_addr_t	cli_addr2 = 3232235522; // 192.168.0.2の３２bit整数表現
+	in_addr_t	cli_addr3 = 3232235523; // 192.168.0.3の３２bit整数表現
+	in_addr_t	cli_addr4 = 3232235524; // 192.168.0.4の３２bit整数表現
+	EXPECT_TRUE(server_config_.allowRequest("first_server",
+											"127.0.0.1",
+											8001,
+											"",
+											cli_addr1));
+	EXPECT_FALSE(server_config_.allowRequest("first_server",
+											"127.0.0.1",
+											8001,
+											"hello/"));
+	EXPECT_TRUE(server_config_.allowRequest("second_server",
+											"127.0.0.2",
+											8002,
+											"",
+											cli_addr1));
+	EXPECT_FALSE(server_config_.allowRequest("second_server",
+											"127.0.0.2",
+											8002,
+											"hello",
+											cli_addr2));
+	EXPECT_FALSE(server_config_.allowRequest("third_server",
+											"192.168.0.3",
+											8003,
+											"hello",
+											cli_addr3));
+	EXPECT_TRUE(server_config_.allowRequest("third_server",
+											"127.0.0.3",
+											8003,
+											"hello",
+											cli_addr4));
 }
 
 TEST_F(ServerConfigTest, getFile)
