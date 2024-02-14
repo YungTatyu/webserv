30675814e7a1ede6b8cb24851a26a2d2bd78707c
kohshi54 <kohshi54.yam@gmail.com>
2024-02-14 17:54:42 +0900

    main back with config

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,19 +6,19 @@
 
 int main(int ac, char *av[])
 {
-	(void)ac, (void)av;
-	// if (ac != 2)
-	// {
-	// 	std::cerr << "Usage: " << av[0] << " [config_file_name]" << std::endl;
-	// 	return 1;
-	// }
-	//
-	// const config::Main *config = config::init_config(av[1]);
-	// if (config == NULL)
-	// 	return 1;
-	//
+	if (ac != 2)
+	{
+		std::cerr << "Usage: " << av[0] << " [config_file_name]" << std::endl;
+		return 1;
+	}
+
+	const config::Main *config = config::init_config(av[1]);
+	if (config == NULL)
+		return 1;
+
 	WebServer server = WebServer();
 	server.eventLoop();
+	delete config;
 	return 0;
 }
 
