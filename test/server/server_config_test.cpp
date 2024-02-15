d831546c33cda3579f612fa0e686003fb83d28f2
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-15 13:32:05 +0900

    :bug: modify argument

--- test/server/server_config_test.cpp
+++ test/server/server_config_test.cpp
@@ -7,35 +7,39 @@
 #include <fstream>
 #include <stdexcept>
 #include <sys/param.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
+// 各テストで使うconfigをセットするクラス
 class ServerConfigTest : public ::testing::Test {
 protected:
     // 各TESTの前に呼び出されるセットアップメソッド
 	void SetUp() override {
 		std::string		file_path;
-		if (IsSubTest("allowRequest")) {
+		const testing::TestInfo*	test_info = testing::UnitTest::GetInstance()->current_test_info();
+		if (test_info->name() == "allowRequest") {
 			file_path = "test/server/conf_files/allowRequest_test.conf";
 		}
-		else if (IsSubTest("getFile")) {
+		else if (test_info->name() == "getFile") {
 			file_path = "test/server/conf_files/getFile_test.conf";
 		}
-		else if (IsSubTest("getKeepaliveTimeout")) {
+		else if (test_info->name() == "getKeepaliveTimeout") {
 			file_path = "test/server/conf_files/getKeepaliveTimeout_test.conf";
 		}
-		else if (IsSubTest("getSendTimeout")) {
+		else if (test_info->name() == "getSendTimeout") {
 			file_path = "test/server/conf_files/getSendTimeout_test.conf";
 		}
-		else if (IsSubTest("getUseridExpires")) {
+		else if (test_info->name() == "getUseridExpires") {
 			file_path = "test/server/conf_files/getUseridExpires_test.conf";
 		}
-		else if (IsSubTest("writeAcsLog")) {
+		else if (test_info->name() == "writeAcsLog") {
 			file_path = "test/server/conf_files/writeAcsLog_test.conf";
 		}
-		else if (IsSubTest("writeErrLog")) {
+		else if (test_info->name() == "writeErrLog") {
 			file_path = "test/server/conf_files/writeErrLog_test.conf";
 		}
 		else {
-			SKIP();
+			GTEST_SKIP();
 		}
 		config::Main	*config = new config::Main();
 		config::Lexer	lexer(file_path);
@@ -53,46 +57,86 @@ protected:
 		delete server_config_.config_;
     }
 
-	// テストに使う共有のオブジェクト
+	// テストに使うオブジェクト
 	ServerConfig	server_config_;
+	struct VServer	vserver_;
+};
+
+namespace test {
+// 実際に使うallowRequestはソケットをつくらないと使えないので、ここではソケットの代わりに、アドレスを渡している。
+bool	allowRequestIPv4( const struct VServer& server_config,
+					const std::string& server_name,
+					const std::string& uri,
+					const in_addr cli_addr );
+// writeAcs/ErrLogのテスト用
+bool	WRITE_ACCURATE( const std::string file, const std::string& phrase ) {
+	std::ifstream	ifs( file );
+	if (!ifs.is_open()) {
+		std::cerr << "file open failed." << std::endl;
+		return false;
+	}
+
+	std::ostringstream content_stream;
+	content_stream << ifs.rdbuf();
+	std::string content = content_stream.str();
+
+	return ( content.find(phrase) != std::string::npos );
+}
 };
 
 // allowRequestの引数プラスでclient自身のアドレス必要かも
 TEST_F(ServerConfigTest, allowRequest)
 {
-	in_addr_t	cli_addr1 = 3232235521; // 192.168.0.1の３２bit整数表現
-	in_addr_t	cli_addr2 = 3232235522; // 192.168.0.2の３２bit整数表現
-	in_addr_t	cli_addr3 = 3232235523; // 192.168.0.3の３２bit整数表現
-	in_addr_t	cli_addr4 = 3232235524; // 192.168.0.4の３２bit整数表現
-	EXPECT_TRUE(server_config_.allowRequest("first_server",
-											"127.0.0.1",
-											8001,
-											"",
-											cli_addr1));
-	EXPECT_FALSE(server_config_.allowRequest("first_server",
-											"127.0.0.1",
-											8001,
-											"hello/"));
-	EXPECT_TRUE(server_config_.allowRequest("second_server",
-											"127.0.0.2",
-											8002,
-											"",
-											cli_addr1));
-	EXPECT_FALSE(server_config_.allowRequest("second_server",
-											"127.0.0.2",
-											8002,
-											"hello",
-											cli_addr2));
-	EXPECT_FALSE(server_config_.allowRequest("third_server",
-											"192.168.0.3",
-											8003,
-											"hello",
-											cli_addr3));
-	EXPECT_TRUE(server_config_.allowRequest("third_server",
-											"127.0.0.3",
-											8003,
-											"hello",
-											cli_addr4));
+	struct in_addr	cli_addr1
+	struct in_addr	cli_addr2;
+	struct in_addr	cli_addr3;
+	struct in_addr	cli_addr4;
+
+	// 初期化
+	inet_pton(AF_INET, "192.168.0.1", &cli_addr1);
+	inet_pton(AF_INET, "192.168.0.2", &cli_addr2);
+	inet_pton(AF_INET, "192.168.0.3", &cli_addr3);
+	inet_pton(AF_INET, "192.168.0.4", &cli_addr4);
+
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
+	// allow all
+	EXPECT_TRUE(test::allowRequestIPv4(vserver_,
+									"first_server",
+									"/",
+									cli_addr1));
+	// deny all
+	EXPECT_FALSE(test::allowRequestIPv4(vserver_,
+									"first_server",
+									"/hello/",
+									cli_addr1));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
+	// allow cli_addr2
+	EXPECT_TRUE(test::allowRequestIPv4(vserver_,
+									"second_server",
+									"/",
+									cli_addr2));
+	// deny cli_addr2
+	EXPECT_FALSE(test::allowRequestIPv4(vserver_,
+									"second_server",
+									"/hello",
+									cli_addr2));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[2]);
+	// deny cli_addr3 
+	EXPECT_FALSE(test::allowRequestIPv4(vserver_,
+									"third_server",
+									"/",
+									cli_addr3));
+	// allow cli_addr4
+	EXPECT_TRUE(test::allowRequestIPv4(vserver_,
+									"third_server",
+									"/hello",
+									cli_addr4));
 }
 
 TEST_F(ServerConfigTest, getFile)
@@ -111,114 +155,115 @@ TEST_F(ServerConfigTest, getFile)
 	//absolutepath = ~/webserv/html
 	absolutepath = static_cast<std::string>(absolute_path);
 
+
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
 	// indexのみ設定されているケース
 	EXPECT_EQ(absolutepath + "/index.html",
-			server_config_.getFile("first_server",
-									"127.0.0.1",
-									8001,
+			server_config_.getFile(vserver_,
+									"first_server",
 									"/"));
 	// indexとtry_filesが設定されているケース
 	EXPECT_EQ(absolutepath + "/test/try.html",
-			server_config_.getFile("first_server",
-									"127.0.0.1",
-									8001,
+			server_config_.getFile(vserver_,
+									"first_server",
 									"/test/"));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
 	// index,try_files.returnが設定されているケース
 	EXPECT_EQ(absolutepath + "/404.html",
-			server_config_.getFile("second_server",
-									"127.0.0.2",
-									8002,
+			server_config_.getFile(vserver_,
+									"second_server",
 									"/test"));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[2]);
 	// location内でrootが変わっているケース
 	EXPECT_EQ(absolutepath + "/change/change_root.html",
-			server_config_.getFile("third_server",
-									"127.0.0.3",
-									8003,
+			server_config_.getFile(vserver_,
+									"third_server",
 									"/"));
 	// aliasでrootが変わるケース
 	EXPECT_EQ(absolutepath + "/alias/alias.html",
-			server_config_.getFile("third_server",
-									"127.0.0.3",
-									8003,
+			server_config_.getFile(vserver_,
+									"third_server",
 									"/test"));
 }
 
 TEST_F(ServerConfigTest, getKeepaliveTimeout)
 {
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
 	// http set
-	EXPECT_EQ(server_config_.http.keepalive_timeout.getTime(),
-			server_config_.getKeepaliveTimeout("first_server",
-											"127.0.0.1",
-											8001,
+	EXPECT_EQ(server_config_.config_->http.keepalive_timeout.getTime(),
+			server_config_.getKeepaliveTimeout(vserver_,
+											"first_server",
 											"/"));
 	// location set
-	EXPECT_EQ(server_config_.http.server_list[0].location_list[1].keepalive_timeout.getTime(),
-			server_config_.getKeepaliveTimeout("first_server",
-											"127.0.0.1",
-											8001,
+	EXPECT_EQ(server_config_.config_->http.server_list[0].location_list[1].keepalive_timeout.getTime(),
+			server_config_.getKeepaliveTimeout(vserver_,
+											"first_server",
 											"/hello"));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
 	// server set
-	EXPECT_EQ(server_config_.http.server_list[1].keepalive_timeout.getTime(),
-			server_config_.getKeepaliveTimeout("second_server",
-											"127.0.0.2",
-											8002,
+	EXPECT_EQ(server_config_.config_->http.server_list[1].keepalive_timeout.getTime(),
+			server_config_.getKeepaliveTimeout(vserver_,
+											"second_server",
 											"/"));
 }
 
 TEST_F(ServerConfigTest, getSendTimeout)
 {
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
 	// default time
-	EXPECT_EQ(server_config_.http.send_timeout.getTime(),
-			server_config_.getSendTimeout("first_server",
-										"127.0.0.1",
-										8001,
+	EXPECT_EQ(server_config_.config_->http.send_timeout.getTime(),
+			server_config_.getSendTimeout(vserver_,
+										"first_server",
 										"/"));
 	// location set
-	EXPECT_EQ(server_config_.http.server_list[0].location_list[1].send_timeout.getTime(),
-			server_config_.getSendTimeout("first_server",
-										"127.0.0.1",
-										8001,
+	EXPECT_EQ(server_config_.config_->http.server_list[0].location_list[1].send_timeout.getTime(),
+			server_config_.getSendTimeout(vserver_,
+										"first_server",
 										"/hello"));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
 	// server set
-	EXPECT_EQ(server_config_.http.server_list[1].send_timeout.getTime(),
-			server_config_.getSendTimeout("second_server",
-										"127.0.0.2",
-										8002,
+	EXPECT_EQ(server_config_.config_->http.server_list[1].send_timeout.getTime(),
+			server_config_.getSendTimeout(vserver_,
+										"second_server",
 										"/"));
 }
 
 TEST_F(ServerConfigTest, getUseridExpires)
 {
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
 	// http set
-	EXPECT_EQ(server_config_.http.userid_expires.getTime(),
-			server_config_.getUseridExpires("first_server",
-										"127.0.0.1",
-										8001,
+	EXPECT_EQ(server_config_.config_->http.userid_expires.getTime(),
+			server_config_.getUseridExpires(vserver_,
+										"first_server",
 										"/"));
 	// location set
-	EXPECT_EQ(server_config_.http.server_list[0].location_list[1].userid_expires.getTime(),
-			server_config_.getUseridExpires("first_server",
-										"127.0.0.1",
-										8001,
+	EXPECT_EQ(server_config_.config_->http.server_list[0].location_list[1].userid_expires.getTime(),
+			server_config_.getUseridExpires(vserver_,
+										"first_server",
 										"/hello"));
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
 	// server set
-	EXPECT_EQ(server_config_.http.server_list[1].userid_expires.getTime(),
-			server_config_.getUseridExpires("second_server",
-										"127.0.0.2",
-										8002,
+	EXPECT_EQ(server_config_.config_->http.server_list[1].userid_expires.getTime(),
+			server_config_.getUseridExpires(vserver_,
+										"second_server",
 										"/"));
 }
 
-namespase test {
-bool	WRITE_ACCURATE( const std::vector<std::string> file_pathes, const std::string& phrase ) {
-	std::istringstream	iss;
-	std::string			content;
-
-	iss.str( file );
-	iss >> content;
-	return (content.find(phrase) != std::string::npos );
-}
-};
 
 TEST_F(ServerConfigTest, writeAcsLog)
 {
@@ -238,45 +283,47 @@ TEST_F(ServerConfigTest, writeAcsLog)
 	std::string	file;
 	std::string	msg;
 
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
 	// offというファイルに書き込むのではなく、どこにも書き込まない
 	file = absolutepath + "/off";
 	msg = "aiueo";
-	server_config_.writeAcsLog("first_server",
-							"127.0.0.1",
-							8001,
+	server_config_.writeAcsLog(vserver_,
+							"first_server",
 							"/",
 							msg);
-	EXPECT_FALSE(WRITE_ACCURATE(file, msg));
+	EXPECT_FALSE(test::WRITE_ACCURATE(file, msg));
 
 	// ロケーションブロックで指定されたところへ出力
 	file = absolutepath + "/logs/location.log";
 	msg = "kakikukeko";
-	server_config_.writeAcsLog("first_server",
-							"127.0.0.1",
-							8001,
+	server_config_.writeAcsLog(vserver_,
+							"first_server",
 							"/hello",
 							msg);
-	EXPECT_TRUE(WRITE_ACCURATE(files, msg));
+	EXPECT_TRUE(test::WRITE_ACCURATE(file, msg));
 
 	// 親ブロックで指定されたファイルに出力
 	file = absolutepath + "/logs/server.log";
 	msg = "sashisuseso";
-	server_config_.writeAcsLog("first_server",
-							"127.0.0.1",
-							8001,
+	server_config_.writeAcsLog(vserver_,
+							"first_server",
 							"/goodnight",
 							msg);
-	EXPECT_TRUE(WRITE_ACCURATE(files, msg));
+	EXPECT_TRUE(test::WRITE_ACCURATE(file, msg));
 
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
 	// 親の親ブロックで指定されたファイルに出力
 	file = absolutepath + "/logs/http.log";
 	msg = "tachitsuteto";
-	server_config_.writeAcsLog("second_server",
-							"127.0.0.2",
-							8002,
+	server_config_.writeAcsLog(vserver_,
+							"second_server",
 							"/",
 							msg);
-	EXPECT_TRUE(WRITE_ACCURATE(file, msg));
+	EXPECT_TRUE(test::WRITE_ACCURATE(file, msg));
 }
 
 TEST_F(ServerConfigTest, writeErrLog)
@@ -297,35 +344,38 @@ TEST_F(ServerConfigTest, writeErrLog)
 	std::string	file;
 	std::string	msg;
 
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[0]);
 	// ログオフ
-	file = /dev/null;
+	file = "/dev/null";
 	msg = "aiueo";
-	server_config_.writeAcsLog("first_server",
-							"127.0.0.1",
-							8001,
+	server_config_.writeAcsLog( vserver_,
+							"first_server",
 							"/",
 							msg);
-	EXPECT_FALSE(WRITE_ACCURATE(file, msg));
+	EXPECT_FALSE(test::WRITE_ACCURATE(file, msg));
 
 	// location.logに出力
 	file = absolutepath + "/logs/location.log";
 	msg = "kakikukeko";
-	server_config_.writeAcsLog("first_server",
-							"127.0.0.1",
-							8001,
+	server_config_.writeAcsLog(vserver_,
+							"first_server",
 							"/hello",
 							msg);
-	EXPECT_TRUE(WRITE_ACCURATE(file, msg));
+	EXPECT_TRUE(test::WRITE_ACCURATE(file, msg));
 
+
+	vserver_.tied_servers_.clear();
+	vserver_.tied_servers_.push_back(&server_config_.config_->http.server_list[1]);
 	// default fileに出力
 	file = absolutepath + "/logs/error.log";
 	msg = "sashisuseso";
-	server_config_.writeAcsLog("second_server",
-							"127.0.0.2",
-							8002,
+	server_config_.writeAcsLog(vserver_,
+							"second_server",
 							"/",
 							msg);
-	EXPECT_TRUE(WRITE_ACCURATE(file, msg));
+	EXPECT_TRUE(test::WRITE_ACCURATE(file, msg));
 
 }
 
