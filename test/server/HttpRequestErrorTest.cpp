41d465137c591eff808ac430811876f078de8ce2
kohshi54 <kohshi54.yam@gmail.com>
2024-02-19 13:19:25 +0900

    Add Invalid TEST

--- test/server/HttpRequestErrorTest.cpp
+++ test/server/HttpRequestErrorTest.cpp
@@ -10,3 +10,12 @@ TEST(HttpRequest, ErrorTest1)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
+TEST(HttpRequest, ErrorTest2)
+{
+    //test invalid character in request line
+    std::string rawRequest = "GET\n/ HTTP/1.1\r\n" "\r\n";
+    HttpRequest test = HttpRequest::parseRequest(rawRequest);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
