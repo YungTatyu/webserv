1740a467168f1ba6e1267ba122b97732ec47f571
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-20 16:58:17 +0900

    :zap: allowRequest Base implement

--- src/server/ConfigHandler.cpp
+++ src/server/ConfigHandler.cpp
@@ -20,19 +20,111 @@ int ConfigHandler::getListenQ()
 	return this->listenQ_;
 }
 
+// parseが失敗したときはその情報どう受け取るか
 bool	ConfigHandler::allowRequest( const struct TiedServer& server_config, const HttpRequest& request, const int cli_sock ) const
 {
 	config::Server&	server = searchServerConfig(server_config, server_name);
 	config::Location&	location = searchLocationConfig(server, uri);
 
+	// cli_Sockからアドレス読み取る
+	struct sockaddr_in client_addr;
+    socklen_t client_addrlen = sizeof(client_addr);
+	if (getsockname(cli_sock, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addrlen) != 0)
+	{
+		printError();
+	}
+	std::cout << "" << client_addr.sin_addr.s_addr << std::endl;
+
+	// ------ access の制限 ------
+	// configからアドレス制限ディレクトリのあるcontext探す
+	if (location.directives_set[kDENY])
+	{
+		// 関数分割
+		// 上から順に制限適用する
+			//制限されているアドレスであれば、false
+			//エラーページどのタイミングで返すか？
+		for (int i = 0; i < location.allow_deny_list.size(); i++)
+		{
+			if (addressInLimit())
+			{
+				switch (location.allow_deny_list[i].getAllowDirective()) {
+					case config::ALLOW:
+						return true;
+						break;
+					case config::DENY:
+						return false;
+						break;
+					default:
+						return false;
+						break;
+				}
+			}
+		}
+	}
+	else if (server.directives_set[kDENY])
+	{
+	}
+	else if (this->config_.http.directives_set[kDENY])
+	{
+	}
+
+
+	// ------ method の制限 ------
+	// location内にlimit_except contextあるか？
+	if (location.directives_set(LIMIT_EXCEPT)
+	{
+		// あれば上から適用する
+		for (int i = 0; i < location.limit_except.size(), i++)
+		{
+		// 制限されたメソッドでなければ、スルー
+			if (request.getMethod() == location.limit_except[i].excepted_methods)
+			{
+				// 上から順に適用していく
+				for (int j = 0; j < location.limit_except[i].allow_deny_list.size(), j++)
+				{
+					if (addressInLimits())
+					{
+						switch (location.limit_except[i].allow_deny_list[j].getAccessDirective()) {
+							case config::ALLOW:
+								return true;
+								break;
+							case config::DENY:
+								return false;
+								break;
+							default :
+								return false;
+								break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// 問題なければtrue
+	return true;
 }
 
 
-const std::string&	ConfigHandler::searchFile( const struct TiedServer& server_config, const HttpRequest& request ) const
+	const std::string&	ConfigHandler::searchFile( const struct TiedServer& server_config, const HttpRequest& request ) const
 {
 	config::Server&	server = searchServerConfig(server_config, server_name);
 	config::Location&	location = searchLocationConfig(server, uri);
 
+	// parseが失敗していれば、400 Bad Request
+
+	// allowReuestがfalseなら403 Forbidden
+
+
+	// urlのlocationがあるか探す
+		// なければ上位のcontextから探す
+		// あればlocationから探す
+
+	// ファイルが存在するか
+		// あれば返す
+		// なければ
+			// error_pageあればそれ返す
+			// なければデフォルト404エラーページ返す
 }
 
 void	ConfigHandler::writeAcsLog( const struct TiedServer& server_config, const std::string& server_name, const std::string& uri, const std::string& msg ) const
@@ -40,6 +132,9 @@ void	ConfigHandler::writeAcsLog( const struct TiedServer& server_config, const s
 	config::Server&	server = searchServerConfig(server_config, server_name);
 	config::Location&	location = searchLocationConfig(server, uri);
 
+	// access_logがどのコンテキストにあるか
+	//
+	// access_logのパスすべてに
 }
 void	ConfigHandler::writeErrLog( const struct TiedServer& server_config, const std::string& server_name, const std::string& uri, const std::string& msg ) const
 {
