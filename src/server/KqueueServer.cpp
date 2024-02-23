e2dcdf1ee2099539cb2197a845aeafed1736fbb9
tatyu <tatyu@t.local>
2024-02-23 22:01:06 +0900

    :construction:debuging kqueue server

--- src/server/KqueueServer.cpp
+++ src/server/KqueueServer.cpp
@@ -25,9 +25,12 @@ void	KqueueServer::eventLoop(
 
 		// 発生したイベントをhandleする
 		callEventHandler(conn_manager, event_manager, io_handler, request_handler);
+		std::cout << "events handled\n";
 
 		// 発生したすべてのイベントを削除
 		event_manager->clearAllEvents();
+		std::cout << "active events deleted\n";
+
 	}
 }
 
@@ -44,23 +47,24 @@ bool	KqueueServer::initKqueueServer()
 
 int	KqueueServer::waitForEvent(ConnectionManager*conn_manager, IActiveEventManager *event_manager)
 {
-	static bool	init = true;
+	// static bool	init = true;
 
 	static std::vector<struct kevent>	event_list; // 監視したいevent
 	std::vector<struct kevent>	*active_events =
 		static_cast<std::vector<struct kevent>*>(event_manager->getActiveEvents());
 
 	// event loop1回目はすべてのイベントを監視リストに追加
-	if (init)
-		initKevents(event_list, conn_manager->getConnections());
-	else
-		AddNewEvents(event_list, conn_manager->getConnections());
-	init = false;
+	// if (init)
+	initKevents(event_list, conn_manager->getConnections());
+	// else
+	// 	AddNewEvents(event_list, conn_manager->getConnections());
+	// init = false;
 
 	// 発生したeventをすべて格納できるサイズにする
 	active_events->resize(event_list.size());
 	// TODO: error処理どうするか？ server downさせる？
 	int re = kevent(this->kq_, event_list.data(), event_list.size(), active_events->data(), active_events->size(), NULL);
+	std::cout << "event counts=" << re << "\n";
 	event_manager->setActiveEventsNum(re);
 	return re;
 }
@@ -80,7 +84,8 @@ void	KqueueServer::initKevents(
 	{
 		const int	event_filter = it->second.event == ConnectionData::READ ? EVFILT_READ : EVFILT_WRITE;
 		// TODO: flag詳細調べる
-		EV_SET(&event_list[i], it->first, event_filter, EV_ADD|EV_ENABLE, 0, 0, 0);
+		EV_SET(&event_list[i], it->first, event_filter, EV_ADD|EV_ENABLE|EV_CLEAR, 0, 0, 0);
+		// std::cout << "event addr=" << &event_list[i] << "\n";
 		++i;
 	}
 }
@@ -99,32 +104,37 @@ void	KqueueServer::callEventHandler(
 	// 発生したイベントの数だけloopする
 	for (int i = 0; i < event_manager->getActiveEventsNum(); ++i)
 	{
-		RequestHandler::UPDATE_STATUS	status = RequestHandler::NONE;
+		// RequestHandler::UPDATE_STATUS	status = RequestHandler::NONE;
 		if (event_manager->isReadEvent(static_cast<const void*>(&(active_events[i]))))
-			status = request_handler->handleReadEvent(*io_handler, *conn_manager, active_events[i].ident);
+			request_handler->handleReadEvent(*io_handler, *conn_manager, active_events[i].ident);
+			// status = request_handler->handleReadEvent(*io_handler, *conn_manager, active_events[i].ident);
 		else if (event_manager->isWriteEvent(static_cast<const void*>(&(active_events[i]))))
-			status = request_handler->handleWriteEvent(*io_handler, *conn_manager, active_events[i].ident);
+			request_handler->handleWriteEvent(*io_handler, *conn_manager, active_events[i].ident);
+			// status = request_handler->handleWriteEvent(*io_handler, *conn_manager, active_events[i].ident);
 		else if (event_manager->isErrorEvent(static_cast<const void*>(&(active_events[i]))))
-			status = request_handler->handleErrorEvent(*io_handler, *conn_manager, active_events[i].ident);
+			request_handler->handleErrorEvent(*io_handler, *conn_manager, active_events[i].ident);
+			// status = request_handler->handleErrorEvent(*io_handler, *conn_manager, active_events[i].ident);
 		
 		// kqueueで監視しているイベント情報を更新
-		switch (status)
-		{
-		case RequestHandler::UPDATE_READ:
-			updateEvent(active_events[i], EVFILT_READ);
-			break;
+		// switch (status)
+		// {
+		// case RequestHandler::UPDATE_READ:
+		// 	updateEvent(active_events[i], EVFILT_READ);
+		// 	break;
 
-		case RequestHandler::UPDATE_WRITE:
-			updateEvent(active_events[i], EVFILT_WRITE);
-			break;
+		// case RequestHandler::UPDATE_WRITE:
+		// 	std::cout << "update: write=" << &active_events[i] << "\n";
+		// 	updateEvent(active_events[i], EVFILT_WRITE);
+		// 	break;
 
-		case RequestHandler::UPDATE_CLOSE:
-			deleteEvent(active_events[i]);
-			break;
+		// case RequestHandler::UPDATE_CLOSE:
+		// 	deleteEvent(active_events[i]);
+		// 	break;
 
-		default:
-			break;
-		}	
+		// default:
+		// 	// std::cout << "default\n";
+		// 	break;
+		// }
 	}
 }
 
