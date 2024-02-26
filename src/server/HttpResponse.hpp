6c39b607c216906fbfef4488b1bd83d2a5414217
ayase-mstk <masa1215.hayase@gmail.com>
2024-02-26 22:42:48 +0900

    :bug: delete miss script

--- src/server/HttpResponse.hpp
+++ src/server/HttpResponse.hpp
@@ -25,7 +25,6 @@ class HttpResponse
 		ConfigHandler	config_handler_;
 
 		// utils methods
-		static void	
 		static std::string	createStaticResponse( const std::string& body, const std::string& statusCode, const std::string& contentType );
 		static std::string	autoIndex( const std::string& directoryPath );
 };
