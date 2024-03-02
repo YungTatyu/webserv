0a42865c9a092f63fe41a2e5e56b0a97b6f52b42
tatyu <tatyu@t.local>
2024-03-02 17:14:42 +0900

    :bug:Timer: fd to non constant

--- src/server/Timer.hpp
+++ src/server/Timer.hpp
@@ -6,11 +6,10 @@
 class Timer
 {
 	private:
-		const int	fd_;
+		int	fd_;
 		unsigned long	raw_time_ms_; // directiveに設定されたraw value
 		unsigned long	timeout_ms_; // timeoutの時間 = now + raw_time_ms_
 		Timer();
-		Timer	&operator=(const Timer &other);
 	public:
 		Timer(const int fd, const config::Time &time);
 		~Timer();
