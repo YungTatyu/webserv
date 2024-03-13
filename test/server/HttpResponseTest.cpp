9186bc27bbef9864c5c3759b97ec3cf73d1464e5
ayase-mstk <masa1215.hayase@gmail.com>
2024-03-13 22:33:41 +0900

    :zap: add Return ResponseTest

--- test/server/HttpResponseTest.cpp
+++ test/server/HttpResponseTest.cpp
@@ -17,7 +17,11 @@ protected:
 		const testing::TestInfo*	test_info = testing::UnitTest::GetInstance()->current_test_info();
 		if (static_cast<std::string>(test_info->name()) == "ParseError") {
 			file_path = "test/server/ResponseTestFiles/ParseError.conf";
-		} else {
+		}
+		else if (static_cast<std::string>(test_info->name()) == "Return") {
+			file_path = "test/server/ResponseTestFiles/Return.conf";
+		}
+		else {
 			config::Main	*config = new config::Main();
 			config_handler_.loadConfiguration(config);
 			GTEST_SKIP();
@@ -68,6 +72,7 @@ TEST_F(HttpResponseTest, ParseError)
 	HttpRequest	request;
 
 	request.method = "GET";
+	request.headers["Host"] = "test_server";
 	request.uri = "/";
 	request.version = "HTTP/1.1";
 	request.parseState = HttpRequest::PARSE_ERROR;
@@ -77,6 +82,86 @@ TEST_F(HttpResponseTest, ParseError)
 	// 関数適用
 	std::string response = HttpResponse::generateResponse(request, tied_server, sock, config_handler_);
 
+	// 結果確認
+	ASSERT_TRUE(test::CORRECT_RESPONSE(correct_res, response));
+}
+
+TEST_F(HttpResponseTest, Return)
+{
+	// 共通初期化
+	// 疑似socket作成
+	int sock = 1;
+	// HttpRequest作成
+	HttpRequest	request;
+
+	request.method = "GET";
+	request.version = "HTTP/1.1";
+	request.headers["Host"] = "test_server";
+	request.parseState = HttpRequest::PARSE_COMPLETE;
+	// TiedServer 作成
+	struct TiedServer	tied_server = config_handler_.createTiedServer("127.0.0.1", 8001);
+	// 正解response
+	std::vector<std::string> correct_res;
+	std::string response;
+
+	// test case
+	// return code; の場合
+	// 初期化
+	request.uri = "/code/";
+	correct_res.push_back("HTTP/1.1 418 I'm a teapot");
+	correct_res.push_back("Server: webserv/1");
+	correct_res.push_back("Connection: keep-alive");
+	// 関数適用
+	response = HttpResponse::generateResponse(request, tied_server, sock, config_handler_);
+	// 結果確認
+	ASSERT_TRUE(test::CORRECT_RESPONSE(correct_res, response));
+
+
+
+	// return code text; の場合
+	// 初期化
+	request.uri = "/code-text/";
+	correct_res.clear();
+	correct_res.push_back("HTTP/1.1 404 Not Found");
+	correct_res.push_back("Server: webserv/1");
+	correct_res.push_back("Connection: keep-alive");
+	correct_res.push_back("Content-Type: text/plain");
+	correct_res.push_back("Content-Length: 14");
+	correct_res.push_back("custom message");
+	// 関数適用
+	response = HttpResponse::generateResponse(request, tied_server, sock, config_handler_);
+	// 結果確認
+	ASSERT_TRUE(test::CORRECT_RESPONSE(correct_res, response));
+
+	// return code URL; の場合
+	// 初期化
+	request.uri = "/code-url/";
+	correct_res.clear();
+	correct_res.push_back("HTTP/1.1 307 Temporary Redirect");
+	correct_res.push_back("Server: webserv/1");
+	correct_res.push_back("Connection: keep-alive");
+	correct_res.push_back("Content-Length: 118");
+	correct_res.push_back("Content-Type: text/html");
+	correct_res.push_back("Location: http://127.0.0.1:7000/code-url.html");
+	correct_res.push_back("<html>\r\n<head><title>307 Temporary Redirect</title></head>\r\n<body>\r\n<center><h1>307 Temporary Redirect</h1></center>\r\n");
+	// 関数適用
+	response = HttpResponse::generateResponse(request, tied_server, sock, config_handler_);
+	// 結果確認
+	ASSERT_TRUE(test::CORRECT_RESPONSE(correct_res, response));
+
+	// return URL; の場合
+	// 初期化
+	request.uri = "/url/";
+	correct_res.clear();
+	correct_res.push_back("HTTP/1.1 302 Moved Temporarily");
+	correct_res.push_back("Server: webserv/1");
+	correct_res.push_back("Connection: keep-alive");
+	correct_res.push_back("Content-Length: 92");
+	correct_res.push_back("Content-Type: text/html");
+	correct_res.push_back("Location: http://127.0.0.1:7000/url.html");
+	correct_res.push_back("<html>\r\n<head><title>302 Found</title></head>\r\n<body>\r\n<center><h1>302 Found</h1></center>\r\n");
+	// 関数適用
+	response = HttpResponse::generateResponse(request, tied_server, sock, config_handler_);
 	// 結果確認
 	ASSERT_TRUE(test::CORRECT_RESPONSE(correct_res, response));
 }
