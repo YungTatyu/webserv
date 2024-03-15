c92eb63632e102098b34211cf523f4c6366df6a1
tatyu <tatyu@t.local>
2024-03-15 20:14:02 +0900

    :trash:delete test.cpp

--- test/cgi/cgi_files/executor/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main() {
-    char *args[] = {"document_response.py", NULL};
-    execve("/Users/tatyu/Documents/webserv/test/cgi/cgi_files/executor/document_response.py", args, NULL);
-    perror("execve failed");
-    return 1;
-}
