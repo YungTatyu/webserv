dcb775ba172ab6f28a9cdabae2f5f4ad5d633cd6
kohshi54 <kohshi54.yam@gmail.com>
2024-03-19 20:17:22 +0900

    add error case test

--- test/server/HttpRequestErrorTest.cpp
+++ test/server/HttpRequestErrorTest.cpp
@@ -25,6 +25,16 @@ TEST(HttpRequest, ErrorTest2)
 
 TEST(HttpRequest, ErrorTest3)
 {
+    //test uri not begin with slash
+    std::string rawRequest = "GET uri HTTP/1.1\r\n" "Host: a\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest4)
+{
     //test invalid character in request line
     std::string rawRequest = "GET\n/ HTTP/1.1\r\n" "Host: a\r\n";
     HttpRequest test;
@@ -33,7 +43,7 @@ TEST(HttpRequest, ErrorTest3)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest4)
+TEST(HttpRequest, ErrorTest5)
 {
     //test format error (no uri)
     std::string rawRequest = "GET HTTP/1.1\r\n" "Host: a\r\n";
@@ -43,7 +53,7 @@ TEST(HttpRequest, ErrorTest4)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest5)
+TEST(HttpRequest, ErrorTest6)
 {
     //test format error (no version)
     std::string rawRequest = "GET / \r\n" "Host: a\r\n";
@@ -53,7 +63,7 @@ TEST(HttpRequest, ErrorTest5)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest6)
+TEST(HttpRequest, ErrorTest7)
 {
     //test format error (empty)
     std::string rawRequest = "";
@@ -63,7 +73,7 @@ TEST(HttpRequest, ErrorTest6)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest7)
+TEST(HttpRequest, ErrorTest8)
 {
     //test format error (no status line)
     std::string rawRequest = "\r\n" "Host: a\r\n";
@@ -74,7 +84,7 @@ TEST(HttpRequest, ErrorTest7)
 }
 
 
-TEST(HttpRequest, ErrorTest8)
+TEST(HttpRequest, ErrorTest9)
 {
     //test format error (no \n at the end of status line)
     std::string rawRequest = "GET / HTTP/1.1\r" "Host: a\r\n";
@@ -84,7 +94,7 @@ TEST(HttpRequest, ErrorTest8)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest9)
+TEST(HttpRequest, ErrorTest10)
 {
     //test format error (incomplete method name)
     std::string rawRequest = "GE / HTTP/1.1\r\n" "Host: a\r\n";
@@ -94,7 +104,7 @@ TEST(HttpRequest, ErrorTest9)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest10)
+TEST(HttpRequest, ErrorTest11)
 {
     //test format error (incomplete method name)
     std::string rawRequest = "GETT / HTTP/1.1\r\n" "Host: a\r\n";
@@ -104,7 +114,7 @@ TEST(HttpRequest, ErrorTest10)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest11)
+TEST(HttpRequest, ErrorTest12)
 {
     //test format error (no method)
     std::string rawRequest = " / HTTP/1.1\r\n" "Host: a\r\n";
@@ -114,20 +124,10 @@ TEST(HttpRequest, ErrorTest11)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest12)
+TEST(HttpRequest, ErrorTest13)
 {
     //test format error (get method is not uppercase, (method is case-sensitive))
     std::string rawRequest = "get / HTTP/1.1\r\n" "Host: a\r\n";
-    HttpRequest test;
-    HttpRequest::parseRequest(rawRequest, test);
-
-    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
-}
-
-TEST(HttpRequest, ErrorTest13)
-{
-    //test format error (version is not uppercase, (version is case-sensitive))
-    std::string rawRequest = "GET / http/1.1\r\n" "Host: a\r\n";
     HttpRequest test;
     HttpRequest::parseRequest(rawRequest, test);
 
@@ -144,11 +144,21 @@ TEST(HttpRequest, ErrorTest14)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
+TEST(HttpRequest, ErrorTest15)
+{
+    //test format error (version is not uppercase, (version is case-sensitive))
+    std::string rawRequest = "GET / http/1.1\r\n" "Host: a\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
 /* --------------  request line error test end -------------- */
 
 /* --------------  header field error test -------------- */
 
-TEST(HttpRequest, ErrorTest15)
+TEST(HttpRequest, ErrorTest16)
 {
     //test: no Host header field (Host header filed is necessary)
     std::string rawRequest = "GET / HTTP/1.1\r\n" "\r\n";
@@ -158,20 +168,10 @@ TEST(HttpRequest, ErrorTest15)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest16)
+TEST(HttpRequest, ErrorTest17)
 {
     //test: duplicate header filed (header name should be unique)
     std::string rawRequest = "GET / HTTP/1.1\r\n" "Host: example.com\r\nHost: aa\r\n";
-    HttpRequest test;
-    HttpRequest::parseRequest(rawRequest, test);
-
-    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
-}
-
-TEST(HttpRequest, ErrorTest17)
-{
-    //test: duplicate header filed (header name should be unique with case insensitive)
-    std::string rawRequest = "GET / HTTP/1.1\r\n" "Host: example.com\r\nHOST: aa\r\n";
     HttpRequest test;
     HttpRequest::parseRequest(rawRequest, test);
 
@@ -190,6 +190,16 @@ TEST(HttpRequest, ErrorTest18)
 
 TEST(HttpRequest, ErrorTest19)
 {
+    //test: duplicate header filed (header name should be unique with case insensitive)
+    std::string rawRequest = "GET / HTTP/1.1\r\n" "Host: example.com\r\nHOST: aa\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest20)
+{
     //test: format error (header filed shold be headername(no space):(one or multiple spaces)value(one or multiple spaces) 
     //ヘッダーネームの後には空白があってはならない.すぐにコロン.
     std::string rawRequest = "GET / HTTP/1.1\r\n" "Host :example.com\r\n";
@@ -199,7 +209,7 @@ TEST(HttpRequest, ErrorTest19)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
-TEST(HttpRequest, ErrorTest20)
+TEST(HttpRequest, ErrorTest21)
 {
     //test: format error (header name should not start with space)
     std::string rawRequest = "GET / HTTP/1.1\r\n" "Host: example.com\r\n New: aa\r\n";
@@ -210,6 +220,20 @@ TEST(HttpRequest, ErrorTest20)
 }
 
 /* --------------  header field error test end -------------- */
+
+/* -------------- query string error test -------------- */
+
+TEST(HttpRequest, ErrorTest22)
+{
+    //test: format error (no uri and just query string)
+    std::string rawRequest = "GET ?name=valueHTTP/1.1\r\n" "Host: example.com\r\n New: aa\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+/* -------------- query string error test end -------------- */
 
 //Content-Lengthと実際のボディのサイズが正しいかとか？
 //chunkだと？
