71781931ec2b240da47e1dd6fd07f10e908a359b
kohshi54 <kohshi54.yam@gmail.com>
2024-03-19 18:33:11 +0900

    add test case

--- test/server/HttpRequestErrorTest.cpp
+++ test/server/HttpRequestErrorTest.cpp
@@ -21,6 +21,107 @@ TEST(HttpRequest, ErrorTest2)
     EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
 }
 
+TEST(HttpRequest, ErrorTest3)
+{
+    //test format error (no uri)
+    std::string rawRequest = "GET HTTP/1.1\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest4)
+{
+    //test format error (no version)
+    std::string rawRequest = "GET / \r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest5)
+{
+    //test format error (empty)
+    std::string rawRequest = "";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest6)
+{
+    //test format error (no status line)
+    std::string rawRequest = "\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+
+TEST(HttpRequest, ErrorTest7)
+{
+    //test format error (no \n at the end of status line)
+    std::string rawRequest = "GET / HTTP/1.1\r" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest8)
+{
+    //test format error (incomplete method name)
+    std::string rawRequest = "GE / HTTP/1.1\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest9)
+{
+    //test format error (incomplete method name)
+    std::string rawRequest = "GETT / HTTP/1.1\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest10)
+{
+    //test format error (no method)
+    std::string rawRequest = " / HTTP/1.1\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest11)
+{
+    //test format error (get method is not uppercase, (method is case-sensitive))
+    std::string rawRequest = "get / HTTP/1.1\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
+TEST(HttpRequest, ErrorTest12)
+{
+    //test format error (version is not uppercase, (version is case-sensitive))
+    std::string rawRequest = "GET / http/1.1\r\n" "\r\n";
+    HttpRequest test;
+    HttpRequest::parseRequest(rawRequest, test);
+
+    EXPECT_EQ(HttpRequest::PARSE_ERROR, test.parseState);
+}
+
 //Content-Lengthと実際のボディのサイズが正しいかとか？
 //重複するヘッダーがあったらどうするんだっけ
 //リクエストラインでは何がエラー？
