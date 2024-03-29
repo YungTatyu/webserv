e55f85917ec886ab96ac654ef058f960ee8082aa
tatyu <tatyu@t.local>
2024-03-29 16:44:49 +0900

    :recycle:refactor: comment

--- test/cgi/CGIExecutor_test.cpp
+++ test/cgi/CGIExecutor_test.cpp
@@ -69,7 +69,6 @@ namespace test
 
 	void	sendBody(const std::string& body, const int socket)
 	{
-		std::cerr << "body:" << body << "\n";
 		Utils::wrapperWrite(socket, body);
 	}
 
@@ -125,42 +124,6 @@ namespace test
 		return *result;
 	}
 } // namespace test
-
-// TEST(cgi_executor, meta_vars)
-// {
-// 	cgi::CGIHandler	cgi_handler;
-// 	HttpRequest	request = test::initRequest(
-// 		"GET",
-// 		"/path/uri/",
-// 		"HTTP/1.1",
-// 		"one=1&two=2&three=3",
-// 		"this is body\n",
-// 		{
-// 			{"Host", "tt"},
-// 			{"content-type", "text"},
-// 			{"CONTENT_LENGTH", "10"}
-// 		}
-// 	);
-
-// 	const std::vector<const char*>	meta_vars = cgi_handler.getCgiExecutor().getMetaVars();
-// 	test::testMetaVars({
-// 		{test::searchMetaVar(meta_vars, "AUTH_TYPE"), "AUTH_TYPE="}, // AUTH_TYPE
-// 		{test::searchMetaVar(meta_vars, "CONTENT_LENGTH"), (std::string("CONTENT_LENGTH=") + std::to_string(request.body.size()))}, // CONTENT_LENGTH
-// 		{test::searchMetaVar(meta_vars, "CONTENT_TYPE"), "CONTENT_TYPE=text"}, // CONTENT_TYPE
-// 		{test::searchMetaVar(meta_vars, "GATEWAY_INTERFACE"), "GATEWAY_INTERFACE=CGI/1.1"}, // GATEWAY_INTERFACE
-// 		{test::searchMetaVar(meta_vars, "PATH_INFO"), "PATH_INFO="}, // PATH_INFO
-// 		{test::searchMetaVar(meta_vars, "PATH_TRANSLATED"), "PATH_TRANSLATED="}, // PATH_TRANSLATED
-// 		{test::searchMetaVar(meta_vars, "QUERY_STRING"), "QUERY_STRING=one=1&two=2&three=3"}, // QUERY_STRING
-// 		{test::searchMetaVar(meta_vars, "REMOTE_ADDR"), "REMOTE_ADDR=client address"}, // REMOTE_ADDR
-// 		{test::searchMetaVar(meta_vars, "REMOTE_HOST"), "REMOTE_HOST=client address"}, // REMOTE_HOST
-// 		{test::searchMetaVar(meta_vars, "REQUEST_METHOD"), "REQUEST_METHOD=GET"}, // REQUEST_METHOD
-// 		{test::searchMetaVar(meta_vars, "SCRIPT_NAME"), "SCRIPT_NAME=/path/uri/"}, // SCRIPT_NAME
-// 		{test::searchMetaVar(meta_vars, "SERVER_NAME"), "SERVER_NAME=tt"}, // SERVER_NAME
-// 		{test::searchMetaVar(meta_vars, "SERVER_PORT"), "SERVER_PORT="}, // SERVER_PORT
-// 		{test::searchMetaVar(meta_vars, "SERVER_PROTOCOL"), "SERVER_PROTOCOL=HTTP/1.1"}, // SERVER_PROTOCOL
-// 		{test::searchMetaVar(meta_vars, "SERVER_SOFTWARE"), "SERVER_SOFTWARE=webserv/1.0"} // SERVER_SOFTWARE
-// 	});
-// }
 
 TEST(cgi_executor, document_response)
 {
