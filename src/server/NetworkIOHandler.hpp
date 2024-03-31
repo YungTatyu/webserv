cef09b9378d89eec62efcdb83096efb34ba93efa
ayase-mstk <masa1215.hayase@gmail.com>
2024-03-31 14:18:06 +0900

    :zap: applyed constants to TiedServer constructor

--- src/server/NetworkIOHandler.hpp
+++ src/server/NetworkIOHandler.hpp
@@ -13,6 +13,7 @@
 # include <fcntl.h>
 # include "SysCallWrapper.hpp"
 # include "Server.hpp"
+# include "Listen.hpp"
 
 #include <string>
 
@@ -26,7 +27,7 @@ struct TiedServer
 	const std::string addr_;
 	const unsigned int port_;
 
-	TiedServer() : addr_("127.0.0.1"), port_(80) {};
+	TiedServer() : addr_(config::Listen::kDefaultAddress_), port_(config::Listen::kDefaultPort_) {};
 	TiedServer( const std::string addr, const unsigned int port ) : addr_(addr), port_(port) {}
 
 	TiedServer&	operator=( const TiedServer& other )
