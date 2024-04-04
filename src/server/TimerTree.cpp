2b2497fe8b3c78c0bd3f3650c9505374d3dbb8aa
ayase-mstk <masa1215.hayase@gmail.com>
2024-04-05 00:21:10 +0900

    :zap: add method which return timeval/timespec in TimerTree

--- src/server/TimerTree.cpp
+++ src/server/TimerTree.cpp
@@ -1,5 +1,6 @@
 #include "TimerTree.hpp"
 
+#include <sys/time.h>
 #include <limits>
 
 TimerTree::TimerTree() {}
@@ -56,6 +57,48 @@ int	TimerTree::findTimer() const
 	const int	timeout = timeout_raw >= static_cast<unsigned long>(std::numeric_limits<int>::max()) ?
 		std::numeric_limits<int>::max() : static_cast<int>(timeout_raw);
 	return timeout;
+}
+
+struct timeval	TimerTree::findTimeval() const
+{
+	struct timeval	tv;
+	const std::multiset<Timer>::iterator it = this->timer_tree_.begin();
+	// timerがない場合は、-1: timeoutなし
+	if (it == this->timer_tree_.end())
+	{
+		tv.tv_sec = -1;
+		tv.tv_usec = -1;
+	}
+
+	// この時点ですでにtimeoutになっている場合は、3msをtimeoutの値として設定する
+	const unsigned long	timeout_raw = it->getTimeout() > Timer::getCurrentTime() ? it->getTimeout() - Timer::getCurrentTime() : 3;
+	// timeoutの値がintmaxを超えている場合は、intmaxを返す
+	tv.tv_sec = timeout_raw >= static_cast<unsigned long>(std::numeric_limits<int>::max()) ?
+		std::numeric_limits<int>::max() / 1000 : static_cast<int>(timeout_raw) / 1000;
+	tv.tv_usec = timeout_raw >= static_cast<unsigned long>(std::numeric_limits<int>::max()) ?
+		(std::numeric_limits<int>::max() % 1000) * 1000 : (static_cast<int>(timeout_raw) % 1000) * 1000;
+	return tv;
+}
+
+struct timespec	TimerTree::findTimespec() const
+{
+	struct timespec	ts;
+	const std::multiset<Timer>::iterator it = this->timer_tree_.begin();
+	// timerがない場合は、-1: timeoutなし
+	if (it == this->timer_tree_.end())
+	{
+		ts.tv_sec = -1;
+		ts.tv_nsec = -1;
+	}
+
+	// この時点ですでにtimeoutになっている場合は、3msをtimeoutの値として設定する
+	const unsigned long	timeout_raw = it->getTimeout() > Timer::getCurrentTime() ? it->getTimeout() - Timer::getCurrentTime() : 3;
+	// timeoutの値がintmaxを超えている場合は、intmaxを返す
+	ts.tv_sec = timeout_raw >= static_cast<unsigned long>(std::numeric_limits<int>::max()) ?
+		std::numeric_limits<int>::max() / 1000 : static_cast<int>(timeout_raw) / 1000;
+	ts.tv_nsec = timeout_raw >= static_cast<unsigned long>(std::numeric_limits<int>::max()) ?
+		(std::numeric_limits<int>::max() % 1000) * 1000 : (static_cast<int>(timeout_raw) % 1000) * 1000000;
+	return ts;
 }
 
 const std::multiset<Timer>	&TimerTree::getTimerTree() const
--- src/server/TimerTree.hpp
+++ src/server/TimerTree.hpp
@@ -17,8 +17,19 @@ class TimerTree
 		void	addTimer(const Timer &timer);
 		void	deleteTimer(const int fd);
 		int	findTimer() const;
+		struct timeval	findTimeval() const;
+		struct timespec	findTimespec() const;
 		const std::multiset<Timer>	&getTimerTree() const;
 		const std::set<int>	&getFdSet() const;
 };
 
 #endif
+
+/* Timerを設定・更新するタイミング
+ * KeepAliveTimeout: ReadEventに変えるとき。
+ *
+ *
+ * Timerを消すタイミング
+ * KeepAliveTimeout: receiveする前、handleErrorEvent
+ *
+ */
