5b73b1314c1abf231bb6953db9662db3009841f0
ayase-mstk <masa1215.hayase@gmail.com>
2024-04-05 22:31:16 +0900

    :recycle: clean code of timerExists

--- src/server/TimerTree.cpp
+++ src/server/TimerTree.cpp
@@ -109,13 +109,5 @@ const std::set<int>	&TimerTree::getFdSet() const
 
 bool	TimerTree::timerExists(const int fd) const
 {
-	for (std::multiset<Timer>::iterator it = this->timer_tree_.begin();
-		it != this->timer_tree_.end();
-		++it
-	)
-	{
-		if(it->getFd() == fd)
-			return true;
-	}
-	return false;
+	return this->fd_set_.find(fd) != this->fd_set_.end();
 }
