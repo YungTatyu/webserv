6143e30e3e92b740b140c712030ff96bef3b8848
tachu <roatyungtatyu@gmail.com>
2024-04-18 16:55:27 +0900

    :seedling: new meta vars test

--- test/cgi/meta_vars_test.py
+++ test/cgi/meta_vars_test.py
@@ -66,11 +66,10 @@ def run_test(conf, req_data):
 
 # テストの引数に渡される
 @pytest.mark.parametrize("conf1", [
-    # "all_meta_vars.conf",
-    "all_meta_vars_poll.conf",
-    "all_meta_vars_select.conf"
+    "all_meta_vars1.conf",
+    "all_meta_vars1_poll.conf",
+    "all_meta_vars1_select.conf"
 ])
-
 # 関数名がtestで始まる関数がテスト実行時に呼ばれる
 def test_all_meta_vars1(conf1):
   run_test(conf1, {
@@ -79,5 +78,21 @@ def test_all_meta_vars1(conf1):
     "body": "this is body message",
     "query_string": "a=a&b=b&c=c",
     "port": 4242,
+    "cgi_file": "all_meta_vars.py"
+  })
+
+
+@pytest.mark.parametrize("conf2", [
+    "all_meta_vars2.conf",
+    "all_meta_vars2_poll.conf",
+    "all_meta_vars2_select.conf"
+])
+def test_all_meta_vars2(conf2):
+  run_test(conf2, {
+    "host": "tachu",
+    "content_type": "text/html",
+    "body": "body body body   body body body   body body body   body body body",
+    "query_string": "location=japan&user=guest",
+    "port": 4343,
     "cgi_file": "all_meta_vars.py"
   })
