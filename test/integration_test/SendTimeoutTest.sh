c0d1f428a2a5a9b55f55093216eab6ac278e4e53
ayase-mstk <masa1215.hayase@gmail.com>
2024-04-18 17:20:56 +0900

    :zap::recycle: add select/poll sendtimeout test & clean code

--- test/integration_test/SendTimeoutTest.sh
+++ test/integration_test/SendTimeoutTest.sh
@@ -2,6 +2,7 @@
 
 # init
 readonly SCRIPT_DIR=$(dirname "$0")
+readonly TEST_NAME="Timeout Test"
 make -j -C "${SCRIPT_DIR}/../../"
 if [ $? -eq 1 ]; then
 	echo "Build webserv failed"
@@ -14,18 +15,9 @@ if [ $? -eq 1 ]; then
 	rm "${WEBSERV_PATH}"
 	exit 1
 fi
-readonly CLIENT_TIMEOUT5_PATH="${SCRIPT_DIR}/test_files/TimeoutTestFiles/client_timeout5"
-readonly CLIENT_TIMEOUT10_PATH="${SCRIPT_DIR}/test_files/TimeoutTestFiles/client_timeout10"
-readonly TEST_NAME="Timeout Test"
-readonly OS=$(uname -s)
+readonly CLIENT_TIMEOUT5_PATH="${SCRIPT_DIR}/test_files/TimeoutTestFiles/timeout5"
+readonly CLIENT_TIMEOUT10_PATH="${SCRIPT_DIR}/test_files/TimeoutTestFiles/timeout10"
 
-#if [ ! -e $WEBSERV_PATH ]; then
-#	echo "${WEBSERV_PATH}: command not found"
-#	echo "run \"make\" first to test"
-#	rm "${CLIENT_PATH}"
-#	exit 1
-#fi
-#
 if [ ! -e $CLIENT_TIMEOUT5_PATH ]; then
 	echo "${CLIENT_TIMEOUT5_PATH}: command not found"
 	echo "run \"make\" first to test"
@@ -68,10 +60,9 @@ function	clean {
 function	runServer {
 	local conf=$1
 	$WEBSERV_PATH "$conf" > /dev/null 2>&1 &
+	# debug 出力する場合
 	#$WEBSERV_PATH "$conf" &
 	sleep 1
-	# エラー出力する場合
-	# $WEBSERV_PATH $1 > /dev/null &
 	WEBSERV_PID=$!
 }
 
@@ -81,10 +72,9 @@ function	runClient {
 	local server_port=$3
 	local request=$4
 	local sleep_time=$5
-	$CLIENT_PATH "$server_ip" "$server_port" "$request" "$sleep_time" > /dev/null 2>&1 &
+	${client_executable} "$server_ip" "$server_port" "$request" "$sleep_time" > /dev/null 2>&1 &
+	# debug 出力する場合
 	#${client_executable} "$server_ip" "$server_port" "$request" "$sleep_time"
-	# エラー出力する場合
-	# $WEBSERV_PATH $1 > /dev/null &
 	CLIENT_PID=$!
 }
 
@@ -96,7 +86,7 @@ function	assert {
 	local	executable_name=$5
 	local	scheme="http"
 	local	host="127.0.0.1"
-	local	port="4242"
+	local	port="4200"
 	local	url="$scheme://$host:$port$uri"
 	local	request=$(cat <<EOT
 GET ${uri} HTTP/1.1
@@ -107,45 +97,35 @@ EOT
 
 	((TOTAL_TESTS++))
 	printf "[  test$TOTAL_TESTS  ]\n${url}: "
-	echo "executable_name is ${executable_name}"
 
-	#(printf "$request"; sleep $(bc <<< "$expect_sec + 5")) | telnet $host $port > /dev/null 2>&1 &
+	# program 実行
 	runClient "${client_executable}" "${host}" "${port}" "${request}" "${expect_sec}" &
+	sleep $(bc <<< "$expect_sec + 1.5")
 
-	# telnetがタイムアウトして1秒以内の誤差であればTRUE
-	#sleep 1
-	#ps | grep -q "${executable_name}"
-	#local client_running=$?
-	#if [ "$client_running" -eq 1 ]; then
-	#	printf "${RED}failed.${RESET}\nmaybe you cannot send request correctly.\n"
-	#	((FAILED_TESTS++))
-	#	return 1
-	#fi
+	# 判定
 	#ps | grep "${executable_name}" | grep -v grep
-	sleep $(bc <<< "$expect_sec + 2")
-	ps | grep "${executable_name}" | grep -v grep
+	ps | grep "${executable_name}" | grep -v grep > /dev/null 2>&1
 	local client_running=$?
-	echo "client_running is ${client_running}"
-
 	if [ "$client_running" -eq 1 ]; then
 		if [ "$expect_result" = "true" ]; then
 			printf "${GREEN}passed.${RESET}\nServer closed the connection\n\n"
 			((PASSED_TESTS++))
 		else
-			printf "${RED}failed.${RESET}\nServer did not timeout\n"
+			printf "${RED}failed.${RESET}\nServer closed the connection\n"
 			((FAILED_TESTS++))
 		fi
 	else # clientが正常にタイムアウトする前にsleepが終了
-		#kill $(ps | grep "${executable_name}" | grep -v grep | cut -d ' ' -f1) > /dev/null 2>&1
-		kill $(ps | grep "${executable_name}" | grep -v grep | cut -d ' ' -f2)
+		kill $(ps | grep "${executable_name}" | grep -v grep | cut -d ' ' -f1) > /dev/null 2>&1
+		#kill $(ps | grep "${executable_name}" | grep -v grep | cut -d ' ' -f1)
 		if [ "$expect_result" = "true" ]; then
 			printf "${RED}failed.${RESET}\nServer did not timeout\n"
 			((FAILED_TESTS++))
 		else
-			printf "${GREEN}passed.${RESET}\nServer closed the connection\n\n"
+			printf "${GREEN}passed.${RESET}\nServer did not timeout\n\n"
 			((PASSED_TESTS++))
 		fi
 	fi
+	sleep 2
 }
 
 function	printLog {
@@ -159,16 +139,16 @@ function	runTest {
 	local	conf=$1
 	local	server_name=$2
 	local	root="test/integration_test/test_files/TimeoutTestFiles"
-	runServer "${root}/${conf}"
 
 	printf "\n${GREEN}<<< ${server_name} server test >>>${RESET}\n"
+	runServer "${root}/${conf}"
+
+	# テスト実行
 	#assert "/" "0" "true"
-	assert "/timeout5/" "5" "true" "${CLIENT_TIMEOUT5_PATH}" "client_timeout5"
-	assert "/timeout10/" "10" "true" "${CLIENT_TIMEOUT10_PATH}" "client_timeout10"
-	assert "/timeout5/" "3" "false" "${CLIENT_TIMEOUT5_PATH}" "client_timeout5"
-	assert "/timeout10/" "8" "false" "${CLIENT_TIMEOUT10_PATH}" "client_timeout10"
-	#assert "/timeout5/" "3" "false"
-	#assert "/timeout10/" "8" "false"
+	assert "/timeout5/" "5" "true" "${CLIENT_TIMEOUT5_PATH}" "timeout5"
+	assert "/timeout10/" "10" "true" "${CLIENT_TIMEOUT10_PATH}" "timeout10"
+	assert "/timeout5/" "3" "false" "${CLIENT_TIMEOUT5_PATH}" "timeout5"
+	assert "/timeout10/" "8" "false" "${CLIENT_TIMEOUT10_PATH}" "timeout10"
 
 	# サーバープロセスを終了
 	kill $WEBSERV_PID > /dev/null 2>&1
@@ -176,9 +156,9 @@ function	runTest {
 
 printf "|------------------ ${TEST_NAME} start ------------------|\n"
 
-runTest "keepalive_timeout.conf" "kqueue or epoll" # kqueue or epoll
-#runTest "keepalive_timeout_select.conf" "select" # select
-#runTest "keepalive_timeout_poll.conf" "poll" # poll
+runTest "send_timeout.conf" "kqueue or epoll" # kqueue or epoll
+runTest "send_timeout_select.conf" "select" # select
+runTest "send_timeout_poll.conf" "poll" # poll
 
 clean
 
