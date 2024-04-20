3a6aff83bfdd1f01782a9e11c4221c37c195a042
YungTatyu <YungTatyu@users.noreply.github.com>
2024-04-20 16:15:11 +0000

    :recycle: auto-format code

--- test/cgi/cgi_files/executor/document_response.py
+++ test/cgi/cgi_files/executor/document_response.py
@@ -2,12 +2,13 @@
 
 import os
 
-def	main():
-      print("content-type: text/html\r\nStatus: 200 OK\r\n\r\n", end="")
-      method = os.environ.get("METHOD")
-      # body = input()
-      # print(body, end="")
+
+def main():
+    print("content-type: text/html\r\nStatus: 200 OK\r\n\r\n", end="")
+    method = os.environ.get("METHOD")
+    # body = input()
+    # print(body, end="")
 
 
 if __name__ == "__main__":
-       main()
+    main()
