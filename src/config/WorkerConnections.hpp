54a90ac9f2908ecb90027abf19b1dacada0e8499
ayase-mstk <masa1215.hayase@gmail.com>
2024-04-21 19:20:29 +0900

    :zap: limit worker_connections when select server

--- src/config/WorkerConnections.hpp
+++ src/config/WorkerConnections.hpp
@@ -12,6 +12,7 @@ class WorkerConnections
 	public:
 		WorkerConnections() : worker_connections_(this->kDefaultWorkerConnections_) {}
 		~WorkerConnections() {}
+		const static unsigned long	kSelectMaxConnections = 1024;
 		const static unsigned long	kDefaultWorkerConnections_ = 512;
 		const unsigned long	&getWorkerConnections() const { return this->worker_connections_; }
 		void	setWorkerConnections(const unsigned long &worker_connections) { this->worker_connections_ = worker_connections; }
