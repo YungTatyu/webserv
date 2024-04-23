19a56b4d1acd12acb569d4c2b048d521d81fca3a
tatyu <tatyu@t.local>
2024-04-23 19:54:24 +0900

    :seedling: new test: bad request

--- test/server/HttpResponse/error/error_page.cpp
+++ test/server/HttpResponse/error/error_page.cpp
@@ -34,5 +34,23 @@ TEST(HttpResponseError, not_found)
 
 TEST(HttpResponseError, bad_request)
 {
+	test::ResponseTest test("test/server/HttpResponse/error/file/not_found.conf");
+	ASSERT_NO_FATAL_FAILURE(test.setUp());
+	test.initTiedServers({
+		{"127.0.0.1", 4242},
+		{"127.0.0.1", 4243}
+	});
+	test.initRequest({{"host", "42"}, {"User-Agent", "Mozilla/5.0"}},
+		"/", HttpRequest::PARSE_ERROR);
+	test.generateResponse();
 
+	test.testHeaders({
+		{"Server", "webserv/1.0"},
+		{"Date", ""},
+		{"Content-Length", std::to_string(test.calcDefaultBodySize(400))},
+		{"Content-Type", "text/html"},
+		{"Connection", "close"},
+	});
+	test.testBody(test.createDefaultErrorBody(400));
+	test.testResponse(test.createResponse(HttpResponse::status_line_map_[400]));
 }
