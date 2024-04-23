44fce1c527ac0400850fe27180783dddbc0ad115
tatyu <tatyu@t.local>
2024-04-23 20:36:20 +0900

    :seedling: new test: forbidden

--- test/server/HttpResponse/error/error_page.cpp
+++ test/server/HttpResponse/error/error_page.cpp
@@ -54,3 +54,26 @@ TEST(HttpResponseError, bad_request)
 	test.testBody(test.createDefaultErrorBody(400));
 	test.testResponse(test.createResponse(HttpResponse::status_line_map_[400]));
 }
+
+TEST(HttpResponseError, forbidden)
+{
+	test::ResponseTest test("test/server/HttpResponse/error/file/not_found.conf");
+	ASSERT_NO_FATAL_FAILURE(test.setUp());
+	test.initTiedServers({
+		{"127.0.0.1", 4242},
+		{"127.0.0.1", 4243}
+	});
+	test.initRequest({{"host", "42"}, {"User-Agent", "Mozilla/5.0"}},
+		"/forbidden/", HttpRequest::PARSE_COMPLETE);
+	test.generateResponse();
+
+	test.testHeaders({
+		{"Server", "webserv/1.0"},
+		{"Date", ""},
+		{"Content-Length", std::to_string(test.calcDefaultBodySize(403))},
+		{"Content-Type", "text/html"},
+		{"Connection", "close"},
+	});
+	test.testBody(test.createDefaultErrorBody(403));
+	test.testResponse(test.createResponse(HttpResponse::status_line_map_[403]));
+}
