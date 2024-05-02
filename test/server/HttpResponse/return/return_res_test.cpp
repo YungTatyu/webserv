594562ac5315b9b2eec8c95e9ad12bbf765318e9
ayase-mstk <masa1215.hayase@gmail.com>
2024-05-02 17:32:22 +0900

    :zap: add return directive response test

---
+++ test/server/HttpResponse/return/return_res_test.cpp
@@ -0,0 +1,66 @@
+#include <gtest/gtest-param-test.h>
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+#include "HttpRequest.hpp"
+#include "HttpResponse.hpp"
+#include "LimitExcept.hpp"
+#include "ResponseTest.hpp"
+
+TEST(HttpResponsereturn, return_code_text) {
+  test::ResponseTest test("test/server/HttpResponse/return/file/return1.conf");
+  ASSERT_NO_FATAL_FAILURE(test.setUpAll({{"127.0.0.1", 4242}, {"127.0.0.1", 4243}},
+                                        {{"host", "someone"}, {"User-Agent", "Mozilla/5.0"}},
+                                        {config::REQUEST_METHOD::GET}, "/text/", HttpRequest::PARSE_COMPLETE));
+
+  const std::string expect_body = "no file";
+  test.testHeaders({
+      {"Server", "webserv/1.0"},
+      {"Date", ""},
+      {"Content-Length", std::to_string(expect_body.size())},
+      {"Content-Type", "text/plain"},
+      {"Connection", "close"},
+  });
+  test.testBody(expect_body);
+  test.testResponse(test.createResponse(HttpResponse::status_line_map_[404]));
+}
+
+TEST(HttpResponsereturn, return_code_and_url) {
+  test::ResponseTest test("test/server/HttpResponse/return/file/return_redirect.conf");
+  ASSERT_NO_FATAL_FAILURE(test.setUpAll({{"127.0.0.1", 4244}, {"127.0.0.1", 4245}},
+                                        {{"host", "mstk"}, {"User-Agent", "Mozilla/5.0"}},
+                                        {config::REQUEST_METHOD::GET}, "/", HttpRequest::PARSE_COMPLETE));
+
+  const std::string expect_body = test.createDefaultErrorBody(303);
+  test.testHeaders({
+      {"Server", "webserv/1.0"},
+      {"Date", ""},
+      {"Content-Length", std::to_string(expect_body.size())},
+      {"Content-Type", "text/html"},
+      {"Connection", "keep-alive"},
+      {"Location", "http://localhost:4242/"},
+  });
+  test.testBody(expect_body);
+  test.testResponse(test.createResponse(HttpResponse::status_line_map_[303]));
+}
+
+TEST(HttpResponsereturn, return_url) {
+  test::ResponseTest test("test/server/HttpResponse/return/file/return_redirect.conf");
+  ASSERT_NO_FATAL_FAILURE(test.setUpAll({{"127.0.0.1", 4242}, {"127.0.0.1", 4243}},
+                                        {{"host", "someone"}, {"User-Agent", "Mozilla/5.0"}},
+                                        {config::REQUEST_METHOD::GET}, "/", HttpRequest::PARSE_COMPLETE));
+
+  const std::string expect_body = test.createDefaultErrorBody(302);
+  test.testHeaders({
+      {"Server", "webserv/1.0"},
+      {"Date", ""},
+      {"Content-Length", std::to_string(expect_body.size())},
+      {"Content-Type", "text/html"},
+      {"Connection", "keep-alive"},
+      {"Location", "http://localhost:4244/"},
+  });
+  test.testBody(expect_body);
+  test.testResponse(test.createResponse(HttpResponse::status_line_map_[302]));
+}
