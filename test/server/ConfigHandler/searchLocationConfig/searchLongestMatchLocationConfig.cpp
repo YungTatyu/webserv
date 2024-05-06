c1b9558f2ae9c4eb59381e05a49d71ec27556839
ayase-mstk <masa1215.hayase@gmail.com>
2024-05-06 17:39:49 +0900

    :zap: add searchLongestMatchLocationConfig test

--- test/server/ConfigHandler/searchLocationConfig/searchLongestMatchLocationConfig.cpp
+++ test/server/ConfigHandler/searchLocationConfig/searchLongestMatchLocationConfig.cpp
@@ -29,21 +29,32 @@ TEST(ConfigHandlerTest, searchLongestMatchLocationConfig_samename)
 TEST(ConfigHandlerTest, searchLongestMatchLocationConfig_longest_match1)
 {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
-  test.initRequest(config::GET, "/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
+  test.initRequest(config::GET, "/everyone/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
   test.sameLocation(
           server.location_list[0],
-          test.config_handler_.searchLongestMatchLocationConfig(server, "/everyone/"));
+          test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
 }
 
 TEST(ConfigHandlerTest, searchLongestMatchLocationConfig_longest_match2)
 {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
-  test.initRequest(config::GET, "/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
+  test.initRequest(config::GET, "/hello/goodnight/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
   test.sameLocation(
           server.location_list[1],
-          test.config_handler_.searchLongestMatchLocationConfig(server, "/hello/goodnight/"));
+          test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
+}
+
+TEST(ConfigHandlerTest, searchLongestMatchLocationConfig_longest_match3)
+{
+  test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
+  test.initRequest(config::GET, "/helo/goodnight/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
+  config::Server server = test.config_handler_.config_->http.server_list[0];
+
+  test.sameLocation(
+          server.location_list[0],
+          test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
 }
