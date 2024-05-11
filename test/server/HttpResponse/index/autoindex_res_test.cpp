27a7c2744f935b1448c4d888d065db8a239fa2a4
ayase-mstk <masa1215.hayase@gmail.com>
2024-05-11 12:51:14 +0900

    :bug: modify autoindex test

--- test/server/HttpResponse/index/autoindex_res_test.cpp
+++ test/server/HttpResponse/index/autoindex_res_test.cpp
@@ -21,7 +21,7 @@ TEST(HttpResponseAutoindex, in_http_on_and_no_location) {
       {"Date", ""},
       {"Content-Length", std::to_string(339)},
       {"Content-Type", "text/html"},
-      {"Connection", "keep-alive"},
+      {"Connection", "keep-alive"}
   });
   test.testResponse(test.createResponse(HttpResponse::status_line_map_[200]));
   test.testAutoindexEntry(test.responses_[0].body_, 8);
@@ -38,11 +38,10 @@ TEST(HttpResponseAutoindex, in_loc_on) {
       {"Date", ""},
       {"Content-Length", std::to_string(1070)},
       {"Content-Type", "text/html"},
-      {"Connection", "keep-alive"},
+      {"Connection", "keep-alive"}
   });
   test.testResponse(test.createResponse(HttpResponse::status_line_map_[200]));
   test.testAutoindexEntry(test.responses_[0].body_, 8);
-  //std::cout << test.responses_[0].body_ << std::endl;
 }
 
 TEST(HttpResponseAutoindex, in_server_off) {
@@ -57,7 +56,7 @@ TEST(HttpResponseAutoindex, in_server_off) {
       {"Date", ""},
       {"Content-Length", std::to_string(expect_body.size())},
       {"Content-Type", "text/html"},
-      {"Connection", "keep-alive"},
+      {"Connection", "keep-alive"}
   });
   test.testBody(expect_body);
   test.testResponse(test.createResponse(HttpResponse::status_line_map_[403]));
@@ -68,16 +67,14 @@ TEST(HttpResponseAutoindex, in_http_on) {
   ASSERT_NO_FATAL_FAILURE(test.setUpAll(
       {{"127.0.0.1", 4244}, {"127.0.0.1", 4245}}, {{"host", "test"}, {"User-Agent", "curl/7.68.0"}},
       {config::REQUEST_METHOD::GET}, "/", HttpRequest::PARSE_COMPLETE));
-  std::string expect_body = test.createDefaultErrorBody(403);
 
   test.testHeaders({
       {"Server", "webserv/1.0"},
       {"Date", ""},
-      {"Content-Length", 1070},
+      {"Content-Length", std::to_string(1070)},
       {"Content-Type", "text/html"},
-      {"Connection", "keep-alive"},
+      {"Connection", "keep-alive"}
   });
-  test.testBody(expect_body);
   test.testResponse(test.createResponse(HttpResponse::status_line_map_[200]));
   test.testAutoindexEntry(test.responses_[0].body_, 8);
 }
