8ed204ead3b89a96b49cb83fd614b66163b3ac30
ayase-mstk <masa1215.hayase@gmail.com>
2024-05-13 16:18:25 +0900

    :zap: modify try_files test

--- test/server/HttpResponse/try_files/try_files_res_test.cpp
+++ test/server/HttpResponse/try_files/try_files_res_test.cpp
@@ -66,7 +66,7 @@ TEST(HttpResponseTry_files, internal_redirect_dir_nothing) {
   test::ResponseTest test("test/server/HttpResponse/try_files/file/try_files_redirect.conf");
   ASSERT_NO_FATAL_FAILURE(test.setUpAll(
       {{"127.0.0.1", 4244}, {"127.0.0.1", 4245}}, {{"host", "mstk"}, {"User-Agent", "Mozilla/5.0"}},
-      {config::REQUEST_METHOD::GET, config::REQUEST_METHOD::POST}, "/", HttpRequest::PARSE_COMPLETE));
+      {config::REQUEST_METHOD::GET}, "/no-dir/", HttpRequest::PARSE_COMPLETE));
   const std::string expect_body = test.createDefaultErrorBody(500);
   test.testHeaders({
       {"Server", "webserv/1.0"},
@@ -77,6 +77,23 @@ TEST(HttpResponseTry_files, internal_redirect_dir_nothing) {
   });
   test.testBody(expect_body);
   test.testResponse(test.createResponse(HttpResponse::status_line_map_[500]));
+}
+
+TEST(HttpResponseTry_files, internal_redirect_file_nothing) {
+  test::ResponseTest test("test/server/HttpResponse/try_files/file/try_files_redirect.conf");
+  ASSERT_NO_FATAL_FAILURE(test.setUpAll(
+      {{"127.0.0.1", 4244}, {"127.0.0.1", 4245}}, {{"host", "mstk"}, {"User-Agent", "Mozilla/5.0"}},
+      {config::REQUEST_METHOD::GET}, "/no-file/", HttpRequest::PARSE_COMPLETE));
+  const std::string expect_body = test.createDefaultErrorBody(404);
+  test.testHeaders({
+      {"Server", "webserv/1.0"},
+      {"Date", ""},
+      {"Content-Length", std::to_string(expect_body.size())},
+      {"Content-Type", "text/html"},
+      {"Connection", "close"},
+  });
+  test.testBody(expect_body);
+  test.testResponse(test.createResponse(HttpResponse::status_line_map_[404]));
 }
 
 TEST(HttpResponseTry_files, error_code) {
