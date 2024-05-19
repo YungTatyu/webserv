3960b8ddc5991f6678f1ad29fae3e88968523eac
YungTatyu <YungTatyu@users.noreply.github.com>
2024-05-19 14:13:19 +0000

    :recycle: auto-format code

--- test/server/ConfigHandler/RootPath/RootPath.cpp
+++ test/server/ConfigHandler/RootPath/RootPath.cpp
@@ -1,31 +1,28 @@
 #include "ConfigHandlerTest.hpp"
 
-TEST(ConfigHandlerTestSearchRootPath, root)
-{
+TEST(ConfigHandlerTestSearchRootPath, root) {
   test::ConfigHandlerTest test("RootPath/test1.conf");
   test.initRequest(config::GET, "/root/", {{"Host", ""}}, "", HttpRequest::PARSE_COMPLETE);
   const config::Server& server = test.config_handler_.config_->http.server_list[0];
   const config::Location* location = &test.config_handler_.config_->http.server_list[0].location_list[0];
 
   // POST method 以外のメソッドを192.168.0.1以外制限
-  //EXPECT_EQ(test.getAbsolutePath(".") + "alias/", test.searchRootPath(server, location));
+  // EXPECT_EQ(test.getAbsolutePath(".") + "alias/", test.searchRootPath(server, location));
   EXPECT_EQ("/var/www/root", test.config_handler_.searchRootPath(server, location));
 }
 
-TEST(ConfigHandlerTestSearchRootPath, alias)
-{
+TEST(ConfigHandlerTestSearchRootPath, alias) {
   test::ConfigHandlerTest test("RootPath/test1.conf");
   test.initRequest(config::GET, "/image/", {{"Host", ""}}, "", HttpRequest::PARSE_COMPLETE);
   const config::Server& server = test.config_handler_.config_->http.server_list[0];
   const config::Location* location = &test.config_handler_.config_->http.server_list[0].location_list[1];
 
   // POST method 以外のメソッドを192.168.0.1以外制限
-  //EXPECT_EQ(test.getAbsolutePath(".") + "alias/", test.searchRootPath(server, location));
+  // EXPECT_EQ(test.getAbsolutePath(".") + "alias/", test.searchRootPath(server, location));
   EXPECT_EQ("/var/www/data/", test.config_handler_.searchRootPath(server, location));
 }
 
-TEST(ConfigHandlerTestSearchRootPath, server)
-{
+TEST(ConfigHandlerTestSearchRootPath, server) {
   test::ConfigHandlerTest test("RootPath/test1.conf");
   test.initRequest(config::GET, "/no-root/", {{"Host", ""}}, "", HttpRequest::PARSE_COMPLETE);
   const config::Server& server = test.config_handler_.config_->http.server_list[0];
@@ -34,20 +31,18 @@ TEST(ConfigHandlerTestSearchRootPath, server)
   EXPECT_EQ("/var/www/html/server1", test.config_handler_.searchRootPath(server, location));
 }
 
-TEST(ConfigHandlerTestSearchRootPath, http)
-{
+TEST(ConfigHandlerTestSearchRootPath, http) {
   test::ConfigHandlerTest test("RootPath/test1.conf");
   test.initRequest(config::GET, "/", {{"Host", "second-server"}}, "", HttpRequest::PARSE_COMPLETE);
   const config::Server& server = test.config_handler_.config_->http.server_list[1];
   const config::Location* location = nullptr;
 
   // POST method 以外のメソッドを192.168.0.1以外制限
-  //EXPECT_EQ(test.getAbsolutePath(".") + "alias/", test.searchRootPath(server, location));
+  // EXPECT_EQ(test.getAbsolutePath(".") + "alias/", test.searchRootPath(server, location));
   EXPECT_EQ("/var/www/html", test.config_handler_.searchRootPath(server, location));
 }
 
-TEST(ConfigHandlerTestSearchRootPath, relative_path)
-{
+TEST(ConfigHandlerTestSearchRootPath, relative_path) {
   test::ConfigHandlerTest test("RootPath/test1.conf");
   test.initRequest(config::GET, "/relative-root/", {{"Host", ""}}, "", HttpRequest::PARSE_COMPLETE);
   const config::Server& server = test.config_handler_.config_->http.server_list[0];
@@ -56,8 +51,7 @@ TEST(ConfigHandlerTestSearchRootPath, relative_path)
   EXPECT_EQ("test/server/HttpResponse/RootPath/file", test.config_handler_.searchRootPath(server, location));
 }
 
-TEST(ConfigHandlerTestSearchRootPath, no_root_directive)
-{
+TEST(ConfigHandlerTestSearchRootPath, no_root_directive) {
   test::ConfigHandlerTest test("RootPath/test2.conf");
   test.initRequest(config::GET, "/no-directive/", {{"Host", ""}}, "", HttpRequest::PARSE_COMPLETE);
   const config::Server& server = test.config_handler_.config_->http.server_list[0];
@@ -65,4 +59,3 @@ TEST(ConfigHandlerTestSearchRootPath, no_root_directive)
 
   EXPECT_EQ("html", test.config_handler_.searchRootPath(server, location));
 }
-
--- test/server/ConfigHandler/searchLocationConfig/searchLongestMatchLocationConfig.cpp
+++ test/server/ConfigHandler/searchLocationConfig/searchLongestMatchLocationConfig.cpp
@@ -1,60 +1,52 @@
 #include "ConfigHandlerTest.hpp"
 
-//const config::Location* searchLongestMatchLocationConfig(
-//                const config::Server& server_config,
-//                const std::string& uri) const;
+// const config::Location* searchLongestMatchLocationConfig(
+//                 const config::Server& server_config,
+//                 const std::string& uri) const;
 
-TEST(ConfigHandlerTestSearchLocationConfig, root)
-{
+TEST(ConfigHandlerTestSearchLocationConfig, root) {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
   test.initRequest(config::GET, "/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
-  test.sameLocation(
-          server.location_list[0],
-          test.config_handler_.searchLongestMatchLocationConfig(server, "/"));
+  test.sameLocation(server.location_list[0],
+                    test.config_handler_.searchLongestMatchLocationConfig(server, "/"));
 }
 
-TEST(ConfigHandlerTestSearchLocationConfig, samename)
-{
+TEST(ConfigHandlerTestSearchLocationConfig, samename) {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
   test.initRequest(config::GET, "/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
-  test.sameLocation(
-          server.location_list[1],
-          test.config_handler_.searchLongestMatchLocationConfig(server, "/hello/"));
+  test.sameLocation(server.location_list[1],
+                    test.config_handler_.searchLongestMatchLocationConfig(server, "/hello/"));
 }
 
-TEST(ConfigHandlerTestSearchLocationConfig, longest_match1)
-{
+TEST(ConfigHandlerTestSearchLocationConfig, longest_match1) {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
   test.initRequest(config::GET, "/everyone/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
-  test.sameLocation(
-          server.location_list[0],
-          test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
+  test.sameLocation(server.location_list[0],
+                    test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
 }
 
-TEST(ConfigHandlerTestSearchLocationConfig, longest_match2)
-{
+TEST(ConfigHandlerTestSearchLocationConfig, longest_match2) {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
-  test.initRequest(config::GET, "/hello/goodnight/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
+  test.initRequest(config::GET, "/hello/goodnight/", {{"Host", "first_server"}}, "",
+                   HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
-  test.sameLocation(
-          server.location_list[1],
-          test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
+  test.sameLocation(server.location_list[1],
+                    test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
 }
 
-TEST(ConfigHandlerTestSearchLocationConfig, longest_match3)
-{
+TEST(ConfigHandlerTestSearchLocationConfig, longest_match3) {
   test::ConfigHandlerTest test("searchLocationConfig/search_location.conf");
-  test.initRequest(config::GET, "/helo/goodnight/", {{"Host", "first_server"}}, "", HttpRequest::PARSE_COMPLETE);
+  test.initRequest(config::GET, "/helo/goodnight/", {{"Host", "first_server"}}, "",
+                   HttpRequest::PARSE_COMPLETE);
   config::Server server = test.config_handler_.config_->http.server_list[0];
 
-  test.sameLocation(
-          server.location_list[0],
-          test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
+  test.sameLocation(server.location_list[0],
+                    test.config_handler_.searchLongestMatchLocationConfig(server, test.request_.uri));
 }
