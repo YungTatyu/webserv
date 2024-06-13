89629076e3c0d6c5c6bda1435f0fca2ab1046e2e
tatyu <tatyu@t.local>
2024-06-13 18:56:43 +0900

    :bug: ClientMaxBodySize: init size

--- src/config/ClientMaxBodySize.hpp
+++ src/config/ClientMaxBodySize.hpp
@@ -10,9 +10,9 @@ class ClientMaxBodySize {
   Size size_;
 
  public:
-  ClientMaxBodySize() {}
-  ~ClientMaxBodySize() {}
   const static unsigned long kDefaultSize_ = Size::megabytes;  // 1m
+  ClientMaxBodySize() : size_(kDefaultSize_) {}
+  ~ClientMaxBodySize() {}
   const Size &getSize() const { return this->size_; }
   void setSize(const unsigned long &size_in_bytes) { this->size_.size_in_bytes_ = size_in_bytes; }
   const static unsigned int kType_ = CONF_HTTP | CONF_TAKE1 | CONF_UNIQUE;
