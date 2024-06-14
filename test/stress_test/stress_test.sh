acbbeca5a0253a0a8fd1b8eb40958c859041187b
tatyu <tatyu@t.local>
2024-06-14 20:02:12 +0900

    :recycle: refactor

--- test/stress_test/stress_test.sh
+++ test/stress_test/stress_test.sh
@@ -11,7 +11,7 @@ function run_test {
   local script="$1"
   local virtual_client="$2" # リクエストを送るclientの数
   local duration="$3"       # test実施時間
-  k6 run "${DIR_PATH}/${script}" --vus ${virtual_client} --duration ${duration}
+  k6 run "${script}" --vus ${virtual_client} --duration ${duration}
 }
 
 function main {
