2309c5ebe1c7fcdbdbd1acc3eb6b4650bcfb214d
tatyu <tatyu@t.local>
2024-06-15 20:41:35 +0900

    :seedling: HttpRequest: new func: parseHost()

--- src/server/HttpRequest.cpp
+++ src/server/HttpRequest.cpp
@@ -691,7 +691,7 @@ HttpRequest::ParseState HttpRequest::parseHeaders(std::string &rawRequest, HttpR
       case sw_header_done:
         if (ch != '\n') return PARSE_ERROR;
         if (isUniqHeaderDup(request, cur_name)) return PARSE_ERROR;
-        if (Utils::compareIgnoreCase(kHost, cur_name) && !isValidHost(cur_value)) return PARSE_ERROR;
+        if (Utils::compareIgnoreCase(kHost, cur_name) && !parseHost(cur_value, request)) return PARSE_ERROR;
         if (Utils::compareIgnoreCase(kContentLength, cur_name) && !isValidContentLength(cur_value))
           return PARSE_ERROR;
         // headerが重複している場合は、一番初めに登場したものを優先する
@@ -743,6 +743,23 @@ HttpRequest::ParseState HttpRequest::parseHeaders(std::string &rawRequest, HttpR
 
   if (te_it == end_it && cl_it == end_it) return PARSE_COMPLETE;  // bodyなし
   return PARSE_HEADER_DONE;
+}
+
+/**
+ * @brief host headerをhost部とport部に分けて、parseする
+ *
+ * ex) host: localhost:8000  -> localhost, 8000
+ */
+bool HttpRequest::parseHost(std::string &host, HttpRequest &request) {
+  if (!isValidHost(host)) return false;
+  // すでにhostをparse済みの場合は、何もしない
+  if (request.headers.find(kHost) != request.headers.end()) return true;
+  size_t i = host.find(':');
+  if (i != std::string::npos) {
+    request.port_in_host = host.substr(i + 1);
+    host = host.substr(0, i + 1);
+  }
+  return true;
 }
 
 /**
@@ -820,14 +837,17 @@ bool HttpRequest::isUniqHeaderDup(const HttpRequest &request, const std::string 
 bool HttpRequest::isInvalidLetter(unsigned char ch) { return ch <= ' ' || ch == 127; }
 
 /**
- * @brief hostが空またはspaceが含まれているといけない
+ * @brief hostの値が正当かチェック
+ * 値が空はerror
+ * spaceが含まれているとerror
+ * 値が:で始まるとerror
  *
  * @param str
  * @return true
  * @return false
  */
 bool HttpRequest::isValidHost(const std::string &str) {
-  return !str.empty() && str.find(' ') == std::string::npos;
+  return !str.empty() && str.find(' ') == std::string::npos && str.find(':') != 0;
 }
 
 bool HttpRequest::isValidContentLength(const std::string &str) {
