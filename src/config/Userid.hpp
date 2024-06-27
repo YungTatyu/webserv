f1101a0adf7bb5810a4008fd6123a9c04a9cd328
tatyu <tatyu@t.local>
2024-06-27 15:09:52 +0900

    :recycle:refactor: following classes adhere to canonical form  src/config/Userid.hpp  src/config/UseridDomain.hpp  src/config/UseridExpires.hpp  src/config/UseridPath.hpp  src/config/UseridService.hpp

--- src/config/Userid.hpp
+++ src/config/Userid.hpp
@@ -8,10 +8,17 @@ class Userid {
 
  public:
   Userid() : is_userid_on_(this->kIsUseridOn_) {}
+  Userid(const Userid& other) { *this = other; }
   ~Userid() {}
-  const static bool kIsUseridOn_ = false;
+  Userid& operator=(const Userid& other) {
+    if (this != &other) {
+      this->is_userid_on_ = other.is_userid_on_;
+    }
+    return *this;
+  }
   bool getIsUseridOn() const { return this->is_userid_on_; }
   void setIsUseridOn(bool is_userid_on) { this->is_userid_on_ = is_userid_on; }
+  const static bool kIsUseridOn_ = false;
   const static unsigned int kType_ =
       CONF_HTTP | CONF_HTTP_SERVER | CONF_HTTP_LOCATION | CONF_TAKE1 | CONF_UNIQUE;
 };
