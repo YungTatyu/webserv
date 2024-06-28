f4ac1dd5d75c301bc49aea90fb2161ffebadeb59
tatyu <tatyu@t.local>
2024-06-28 20:53:20 +0900

    :recycle:refactor: rename vars

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,13 @@
 #include "WebServer.hpp"
 #include "conf.hpp"
 
-int main(int ac, char *av[]) {
-  if (ac != 2) {
-    std::cerr << "Usage: " << av[0] << " [config_file_name]" << std::endl;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " [config_file_name]" << std::endl;
     return 1;
   }
 
-  const config::Main *config = config::initConfig(av[1]);
+  const config::Main *config = config::initConfig(argv[1]);
   if (config == NULL) return 1;
 
   try {
