bc83c26625a0b3a812f2f3779d0dfd16a17ede2c
ayase-mstk <ayase-mstk@users.noreply.github.com>
2024-07-10 07:25:40 +0000

    :recycle:refactor: auto-format code

--- src/server/ConnectionManager.cpp
+++ src/server/ConnectionManager.cpp
@@ -21,7 +21,8 @@ bool ConnectionManager::setConnection(int fd) {
   const ConfigHandler& config_handler = WebServer::getConfigHandler();
   // selectが扱える最大fd値は1024なので、それを超えていたら切断
   if (config_handler.getPollingMethod() == config::SELECT && FD_SETSIZE <= fd) {
-    WebServer::writeErrorlog("a connection refused because value of fd exceeded " + toStr(config::WorkerConnections::kSelectMaxConnections_),
+    WebServer::writeErrorlog("a connection refused because value of fd exceeded " +
+                                 toStr(config::WorkerConnections::kSelectMaxConnections_),
                              config::EMERG);
     return false;
   }
@@ -42,7 +43,8 @@ bool ConnectionManager::setCgiConnection(int cli_sock, ConnectionData::EVENT eve
   const ConfigHandler& config_handler = WebServer::getConfigHandler();
   // selectが扱える最大fd値は1024なので、それを超えていたらfalse
   if (config_handler.getPollingMethod() == config::SELECT && FD_SETSIZE <= cli_sock) {
-    WebServer::writeErrorlog("a connection refused because value of fd exceeded " + utils::toStr(config::WorkerConnections::kSelectMaxConnections_),
+    WebServer::writeErrorlog("a connection refused because value of fd exceeded " +
+                                 utils::toStr(config::WorkerConnections::kSelectMaxConnections_),
                              config::EMERG);
     return false;
   }
