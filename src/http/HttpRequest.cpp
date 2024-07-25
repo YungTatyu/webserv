e9121ad1e79c8c6831e5bfe27793318b34dd6f8b
Masataka Hayase <mahayase@c3r5s5.42tokyo.jp>
2024-07-25 16:11:38 +0900

    :bug: parseRequest return if PARSE_NOT_IMPLEMENTED

--- src/http/HttpRequest.cpp
+++ src/http/HttpRequest.cpp
@@ -50,7 +50,7 @@ HttpRequest &HttpRequest::operator=(const HttpRequest &other) {
 void HttpRequest::parseRequest(std::string &raw_request, HttpRequest &request) {
   // 新たなリクエストの場合は初期化する
   if (!isParsePending(request)) {
-     HttpRequest::clear(request);
+    HttpRequest::clear(request);
   }
   ParseState &state = request.parse_state_;
   while (state != PARSE_COMPLETE) {
@@ -77,8 +77,7 @@ void HttpRequest::parseRequest(std::string &raw_request, HttpRequest &request) {
       default:
         break;
     }
-    // PARSE_NOT_IMPLEMENTEDは条件に入らなくていいのか？
-    if (state == PARSE_ERROR || state == PARSE_ERROR_BODY_TOO_LARGE) return;
+    if (state == PARSE_ERROR || state == PARSE_ERROR_BODY_TOO_LARGE || state == PARSE_NOT_IMPLEMENTED) return;
     // parse未完了：引き続きクライアントからのrequestを待つ
     if (state == state_before || state == PARSE_INPROGRESS) return;
   }
